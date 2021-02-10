c02d31035816002049a721ef661b27d991b45c6c
Ka-Bis <k.sydaphasavanh@gmail.com>
2021-02-10 11:05:11 +0100

    Ajout du component Navbar.vue dans App.vue

--- cfa-front/src/App.vue
+++ cfa-front/src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
+    <Navbar/>
     <router-view/>
   </div>
 </template>
-
+<script>
+import Navbar from "@/components/Navigation/Navbar.vue"
+export default {
+  name:"Home",
+  components:   {
+    Navbar
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
