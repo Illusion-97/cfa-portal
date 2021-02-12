7b99c0e969436e547c10f12e559e80030f8306e8
Nicolas <npotier@dawan.fr>
2021-02-12 10:19:37 +0100

    test

--- cfa-front/src/router/index.js
+++ cfa-front/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import EspaceAdministratif from '@/components/Etudiant/EspaceAdministratif/EspaceAdministratif.vue'
 import DepotFichier from '@/components/Etudiant/EspaceAdministratif/DepotFichier.vue'
 import DemandeConge from '@/components/Etudiant/EspaceAdministratif/DemandeConge.vue'
+import LoginPage from '@/components/Login/LoginPage.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,7 @@ const routes = [
   { path: '/Etudiant/EspaceAdministratif', name: 'EspaceAdministratif', component: EspaceAdministratif},
   { path: '/Etudiant/EspaceAdministratif/DepotFichier', name: 'DepotFichier', component: DepotFichier},
   { path: '/Etudiant/EspaceAdministratif/DemandeConge', name: 'DemandeConge', component: DemandeConge},
+  { path: '/Login/LoginPage', name: 'LoginPage', component: LoginPage},
 ]
 
 const router = new VueRouter({
