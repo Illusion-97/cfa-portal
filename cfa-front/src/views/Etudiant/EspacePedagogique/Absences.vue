297dff116ee55867ae5c22e6ec16c650c9e35fef
aatia <aatia@dawan.fr>
2021-05-05 14:29:51 +0200

    modif de la partie peda (test)

--- cfa-front/src/views/Etudiant/EspacePedagogique/Absences.vue
+++ cfa-front/src/views/Etudiant/EspacePedagogique/Absences.vue
@@ -7,11 +7,18 @@
         <BodyTitle title="Mes absences et retards" />
 
         <br>
-        <br>
-        <br>
-
+        
         <div class="Menu">
-            <p>censé y avoir un truc la</p>
+          <TableTemplate 
+          :perPage="perPage"
+          :items="items"
+          :fields="fields"
+          :showBtn="false"/>
+          <TableTemplate 
+          :perPage="perPage"
+          :items="items"
+          :fields="fields"
+          :showBtn="false"/>
         </div>
     </div>
     </div>
@@ -21,10 +28,36 @@
 </template>
 <script>
 import BodyTitle from "@/components/utils/BodyTitle.vue";
+import TableTemplate from "@/components/utils/TableTemplate.vue";
 export default {
   name: "Abscences",
   components: {
-    BodyTitle
-  }
+    BodyTitle,
+    TableTemplate,
+  },
+  data() {
+    return {
+      perPage: 10,
+      items: [
+        {
+          Date: " Du 13/05/2020 au 13/05/2020",
+          Motif: "COVID",
+          Justificatif: "",
+        }, 
+        {
+          Date: " Du 13/04/2021 au 18/04/2021",
+          Motif: "Operation",
+          Justificatif: "",
+        }, 
+        {
+          Date: " Du 13/05/2020 au 13/05/2020",
+          Motif: "COVID",
+          Justificatif: "",
+        },
+       
+      ],
+    };
+  },
+  
 }
 </script>
