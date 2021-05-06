149a6ff4d82666885e4fb9479ff6dfc74ee160c0
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-05-06 11:47:04 +0200

    update Navbar

--- cfa-front/src/components/Navigation/Navbar.vue
+++ cfa-front/src/components/Navigation/Navbar.vue
@@ -10,45 +10,49 @@
       <b-navbar-toggle target="nav-collapse" class="border-0"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
-        
+
         <b-navbar-nav>
           <b-nav-item href="/" class=" a-link">Home</b-nav-item>
-              <b-nav-item class=" a-link" href="/etudiant/espace-pedagogique/acceuil">
-                  Espace pedagogique
-              </b-nav-item>
-              <b-nav-item class=" a-link" href="/etudiant/espace-administratif/profil">
-                  Espace administrative
+          <b-nav-item class=" a-link" href="/etudiant/espace-pedagogique/acceuil">
+            Espace pedagogique
           </b-nav-item>
-        </b-navbar-nav>  
+          <b-nav-item class=" a-link" href="/etudiant/espace-administratif/profil">
+            Espace administrative
+          </b-nav-item>
+          <!-- <router-link tag="li" :to="{name:'referent_dashboard'}" class="a-link">
+            <a class="text-white">TEST</a>
+          </router-link> -->
+
+        </b-navbar-nav>
       </b-collapse>
     </b-navbar>
   </header>
 </template>
 
 <script>
-export default {
-  name: "Navbar",
-  components: {
-  },
-  methods: {
-    setActiv(path){
-      if(path == this.$router.currentRoute.fullPath)
-        return 'active';
-    }
-  },
-  computed: {
-    table : function(){
-      return this.$router.currentRoute.fullPath.split('/').splice(1);
+  export default {
+    name: "Navbar",
+    components: {},
+    methods: {
+      setActiv(path) {
+        if (path == this.$router.currentRoute.fullPath)
+          return 'active';
+      }
     },
-  }
-};
+    computed: {
+      table: function () {
+        return this.$router.currentRoute.fullPath.split('/').splice(1);
+      },
+    }
+  };
 </script>
 
 <style scoped>
   /*@import './assets/styles/NavBar.css';*/
 
   header {
-    margin-bottom: 50px;;
+    margin-bottom: 50px;
+    ;
   }
 
   .navbar {
@@ -57,17 +61,17 @@ export default {
     background-color: #212529;
     /* width: 70%; */
   }
-  
+
   .a-link,
   .nav-form {
     background-color: #212529;
   }
-  
+
   .navbar-light .navbar-nav .nav-link {
     color: #fff;
     font-size: 20px;
   }
-  
+
   .input-search {
     background-color: #212529;
     border: none;
@@ -76,34 +80,34 @@ export default {
     outline: none;
     color: rgba(255, 255, 255, 0.877);
   }
-  
+
   .input-search:focus {
     outline: solid #212529
   }
-  
+
   .input-search::placeholder {
     color: rgb(168, 168, 168);
     font-size: 15px;
   }
-  
+
   .btn-search {
     background-color: rgba(226, 27, 40, 0.65);
     border: none;
     padding-top: 0.3em;
     padding-bottom: 0.3em;
   }
-  
-  
+
+
   @media (max-width: 990px) {
     .navbar {
       width: 100%;
       margin: 0;
     }
-  
+
     #dawan_logo {
       display: none;
     }
-  
+
     .nav-form {
       justify-content: center;
       padding-top: 0.5em;
