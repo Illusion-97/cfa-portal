4fd3a1d7f008336496baa642ca5b743f561e8f25
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-05-10 09:27:02 +0200

    Update Cours.vue

--- cfa-front/src/views/Formateur/Cours/Cours.vue
+++ cfa-front/src/views/Formateur/Cours/Cours.vue
@@ -3,26 +3,18 @@
     <!-- <BodyTitle title="Affichage des cours" /> -->
     <h1 class="text-center">Affichage des cours</h1>
     <TableTemplate :perPage="perPage" :items="items" :fields="fields" btnTxt="Ajouter une intervention" :showBtn="true"
-      btnLink="formateur_ajouter_cours" />
-
-    <!-- {{ items.length +0.5 | formatNumber}} -->
+      btnLink="formateur_ajouter-cours" />
   </div>
 </template>
 
 <script>
   import TableTemplate from "@/components/utils/TableTemplate.vue";
-  // import courseModel from "@/assets/mock/course.json";
-  import {
-    courseFields
-  } from "@/assets/js/fields.js";
-  // import { courseItemLength } from "@/assets/mock/itemLength.js";
-  // import BodyTitle from "@/components/utils/BodyTitle.vue";
+  import { courseFields } from "@/assets/js/fields.js";
   import axios from "axios";
   export default {
     name: "Cours",
     data() {
       return {
-        // items: courseModel,
         items: [],
         fields: courseFields,
         perPage: 10,
@@ -34,23 +26,23 @@
     },
     created() {
       axios
-        .get("http://localhost:8080/AppliCFABack/interventions/with-object")
+        .get(`${this.$apiUrl}/AppliCFABack/interventions/with-object`)
         .then((response) => {
           this.items = response.data;
           console.log(this.items);
         })
         .catch((e) => this.errors.push(e));
     },
-    // methods: {
-    //   items(){
-    //     var result;
-    //      axios
-    //       .get("http://localhost:8080/AppliCFABack/etudiants/23/intervention")
-    //       .then((response) => (this.items = response.data))
-    //       .catch((e) => this.errors.push(e));
-    //       return result
-    //   }
-    // }
+    methods: {
+      // getIntervention(){
+      //   var result;
+      //    axios
+      //     .get("http://localhost:8080/AppliCFABack/etudiants/23/intervention")
+      //     .then((response) => (this.items = response.data))
+      //     .catch((e) => this.errors.push(e));
+      //     return result
+      // }
+    },
   };
 </script>
 
