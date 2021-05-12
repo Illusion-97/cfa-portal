fcd027e1eb3a0d70af07237b23304914462aaa54
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-05-12 12:27:59 +0200

    add detail cours view

---
+++ cfa-front/src/views/Formateur/Cours/DetailCours.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Detail Cours</h1>
+    {{items}}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      interventionId: this.$route.params.id,
+      items: [],
+    };
+  },
+  created() {
+    axios
+      .get(`${this.$apiUrl}/AppliCFABack/interventions/${this.interventionId}/formation`)
+      .then((response) => {
+        this.items = response.data;
+        // console.log(this.items.interventionsDto);
+      })
+      .catch((e) => console.error(e));
+  },
+};
+</script>
+
+<style scoped></style>
