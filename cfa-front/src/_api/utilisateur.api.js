c7877cd4008999f7f606ae9228c4982bd08e71eb
Nicolas <npotier@dawan.fr>
2021-06-08 10:41:11 +0200

    ajout _entities Adresse/Entreprise et _api entreprise et modif de utilisateur

--- cfa-front/src/_api/utilisateur.api.js
+++ cfa-front/src/_api/utilisateur.api.js
@@ -5,10 +5,92 @@ import {constantesApi} from "@/_api/constantes.api.js"
 export const utilisateurApi = {
     getById,
 };
+const END_POINT = "users";
+const ADRESSE = "adresse";
+const ENTREPRISE = "entreprise";
 
 function getById(id) {
     let req =  constantesApi.url + "utilisateurs/" + id;
 
     return fetch(req, requestOptions.get())
         .then(handleResponse);
+}
+
+export async function getAllUsersHttp() {
+    let users = [];
+    const response = await axios.get(`${constantesApi.url}${END_POINT}`, {
+      //headers: { Authorization: $cookies.get("token") },
+    });
+    users = response.data;
+    return users;
+  }
+  export async function getAllUsersBy() {
+    let users = [];
+    const response = await axios.get(`${constantesApi.url}${END_POINT}`, {
+      //headers: { Authorization: $cookies.get("token") },
+    });
+    users = response.data;
+    return users;
+  }
+
+  export async function getAllUsersByEntreprise() {
+    let users = [];
+    const response = await axios.get(`${constantesApi.url}${END_POINT}${ENTREPRISE}`, {
+      //headers: { Authorization: $cookies.get("token") },
+    });
+    users = response.data;
+    return users;
+  }
+
+  export async function getAllUsersByAdresse() {
+    let users = [];
+    const response = await axios.get(`${basconstantesApi.urleUrl}${END_POINT}${ADRESSE}`, {
+      //headers: { Authorization: $cookies.get("token") },
+    });
+    users = response.data;
+    return users;
+  }
+
+export async function addUserHttp(user) {
+  let userAdded = null;
+  const response = await axios.post(
+    `${constantesApi.url}${END_POINT}`,
+    {
+        id: user.id,
+        login: user.login,
+        password: user.password,
+        prenom: user.prenom,
+        nom: user.nom,
+    },
+    {
+      //headers: { Authorization: $cookies.get("token") },
+    }
+  );
+  userAdded = response.data;
+  return userAdded;
+}
+
+export async function updateUserHttp(user) {
+  let userUpdated = null;
+  const response = await axios.post(
+    `${constantesApi.url}${END_POINT}`,
+    {
+      id: user.id,
+      login: user.login,
+      password: user.password,
+      prenom: user.prenom,
+      nom: user.nom,
+    },
+    {
+      //headers: { Authorization: $cookies.get("token") },
+    }
+  );
+  userUpdated = response.data;
+  return userUpdated;
+}
+
+export async function deleteUserHttp(id) {
+  let response = null;
+  response = await axios.delete(`${constantesApi.url}${END_POINT}/${id}`);
+  return response.data;
 }
