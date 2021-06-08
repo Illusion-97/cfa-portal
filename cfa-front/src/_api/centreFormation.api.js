406486ac5d7f40f98a2843c0c458c51cd190e79b
Nicolas <npotier@dawan.fr>
2021-06-08 17:12:18 +0200

    ajout _entities / _api CentreFormation / Projet

---
+++ cfa-front/src/_api/centreFormation.api.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import {constantesApi} from "@/_api/constantes.api.js"
+
+
+const END_POINT = "centreFormations";
+
+
+export async function getAllCentreFormationsHttp() {
+    let centreFormations = [];
+    const response = await axios.get(`${constantesApi.url}${END_POINT}`, {
+      //headers: { Authorization: $cookies.get("token") },
+    });
+    centreFormations = response.data;
+    return centreFormations;
+  }
+  export async function getAllCentreFormationsBy() {
+    let centreFormations = [];
+    const response = await axios.get(`${constantesApi.url}${END_POINT}`, {
+      //headers: { Authorization: $cookies.get("token") },
+    });
+    centreFormations = response.data;
+    return centreFormations;
+  }
+
+
+export async function addCentreFormationsHttp(centreFormation) {
+  let centreFormationAdded = null;
+  const response = await axios.post(
+    `${constantesApi.url}${END_POINT}`,
+    {
+        id: centreFormation.id,
+        adresse: centreFormation.adresse,
+        entreprise: centreFormation.entreprise,
+    },
+    {
+      //headers: { Authorization: $cookies.get("token") },
+    }
+  );
+  centreFormationAdded = response.data;
+  return centreFormationAdded;
+}
+
+export async function updateCentreFormationsHttp(centreFormation) {
+  let centreFormationAdded = null;
+  const response = await axios.post(
+    `${constantesApi.url}${END_POINT}`,
+    {
+        id: centreFormation.id,
+        adresse: centreFormation.adresse,
+        entreprise: centreFormation.entreprise,
+    },
+    {
+      //headers: { Authorization: $cookies.get("token") },
+    }
+  );
+  centreFormationAdded = response.data;
+  return centreFormationAdded;
+}
+
+export async function deleteCentreFormationsHttp(id) {
+  let response = null;
+  response = await axios.delete(`${constantesApi.url}${END_POINT}/${id}`);
+  return response.data;
+}
