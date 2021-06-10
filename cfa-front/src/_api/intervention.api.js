5076c06f63394c13d234767f7bf29bb129c0ae0a
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-06-10 15:05:44 +0200

    insert intervention function

--- cfa-front/src/_api/intervention.api.js
+++ cfa-front/src/_api/intervention.api.js
@@ -1,20 +1,27 @@
 import axios from 'axios';
-
 export const interventionApi = {
     getIntervention,
-    getInterventionById
+    getInterventionById,
+    insertIntervention
 }
 
 function getIntervention(page, size) {
-    const url = `${this.$apiUrl}/AppliCFABack/interventions/${page}/${size}`
+    const url = `${process.env.VUE_APP_API_URL}/interventions/with-object/${page-1}/${size}`
     return axios.get(url)
         .then(response => response.data)
         .catch(err => console.error(err));
 }
 
 function getInterventionById(id) {
-    const url = `${this.$apiUrl}/AppliCFABack/interventions/${id}`
+    const url = `${process.env.VUE_APP_API_URL}/interventions/${id}`
     return axios.get(url)
         .then(response => response.data)
+        .catch(err => console.error(err));
+}
+
+function insertIntervention(form) {
+    const url = `${process.env.VUE_APP_API_URL}/interventions/`;
+    return axios.post(url, form)
+        .then(response => response)
         .catch(err => console.error(err));
 }
