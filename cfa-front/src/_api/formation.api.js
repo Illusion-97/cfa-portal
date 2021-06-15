b7941d0b47446fdcf7752a0e0fe22eeb2ee09ac1
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-06-15 11:34:34 +0200

    formation service + add method in intervention service

--- cfa-front/src/_api/formation.api.js
+++ cfa-front/src/_api/formation.api.js
@@ -1,20 +1,34 @@
 import axios from 'axios'
 export const formationApi = {
     getFormation,
-    insertFormation
+    getAllFormation,
+    insertFormation,
+    getFormationById,
 }
 
 function getFormation(page, size) {
-    const url = `${process.env.VUE_APP_API_URL}/formations/${page-1}/${size}`
+    const url = `${process.env.VUE_APP_API_URL}/formations/${page - 1}/${size}`
+    return axios.get(url)
+        .then(response => response.data)
+        .catch(err => console.error(err))
+}
+function getAllFormation() {
+    const url = `${process.env.VUE_APP_API_URL}/formations`
     return axios.get(url)
         .then(response => response.data)
         .catch(err => console.error(err))
 }
 
-
 function insertFormation(form) {
     const url = `${process.env.VUE_APP_API_URL}/formations`
     return axios.post(url, form)
         .then(response => response)
+        .catch(err => console.error(err))
+}
+
+function getFormationById(id) {
+    const url = `${process.env.VUE_APP_API_URL}/formations/${id}`;
+    return axios.get(url)
+        .then(response => response.data)
         .catch(err => console.error(err))
 }
