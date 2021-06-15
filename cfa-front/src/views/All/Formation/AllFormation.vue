6909eaf461f8e089feb2d75b5db21a5789bf74a8
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-06-15 11:46:50 +0200

    formation form + formation list + formation detail

---
+++ cfa-front/src/views/All/Formation/AllFormation.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <TableTemplate :items="items" :fields="fields" :currentPage="currentPage" :perPage="perPage" />
+    {{ items }}
+  </div>
+</template>
+
+<script>
+  import TableTemplate from "@/components/utils/TableTemplate.vue";
+  import { formationApi } from "@/_api/formation.api.js";
+  import { formationFields } from "@/assets/js/fields.js";
+  export default {
+    name: "Formation",
+    components: {
+      TableTemplate,
+    },
+    data() {
+      return {
+        items: [],
+        fields: formationFields,
+        currentPage: 1,
+        perPage: 10,
+      };
+    },
+    created() {
+      this.fillList();
+    },
+    methods: {
+      fillList() {
+        formationApi
+          .getAllFormation()
+          // .getFormation(this.currentPage, this.perPage)
+          .then((data) => (this.items = data));
+      },
+    },
+  };
+</script>
+
+<style scoped>
+.title {
+  background-color: red;
+}
+
+  </style>
---
+++ cfa-front/src/views/All/Formation/DetailFormation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ formationId }}
+    {{item}}
+  </div>
+</template>
+
+<script>
+import { formationApi } from "../../../_api/formation.api";
+export default {
+  data() {
+    return {
+      formationId: this.$route.params.id,
+      item: [],
+    };
+  },
+  methods: {},
+  created() {
+    formationApi
+      .getFormationById(this.formationId)
+      .then((data) => (this.item = data));
+  },
+};
+</script>
+
+<style></style>
