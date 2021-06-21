58860ff3ec11ceae2eb3d98fb9f6f5395a0e3082
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-06-21 13:26:15 +0200

    formateur api

---
+++ cfa-front/src/_api/formateur.api.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+// import { requestOptions } from '@/_helpers/request-options.js';
+
+const END_POINT = "formateurs";
+
+export const formateurApi = {
+    getAllFormateur,
+    getFormateurById,
+    countFormateurs,
+    getInterventionsByFormateurId,
+    countInterventionsByFormateurId
+}
+
+
+function getAllFormateur(page, size, key = "") {
+    return axios.get(`/${END_POINT}/${page}/${size}/${key}`)
+        .then(response => response.data)
+        .catch(err => console.error(err));
+}
+
+
+function getFormateurById(id) {
+    const url = `${END_POINT}/${id}`
+    return axios.get(url)
+        .then(response => response.data)
+        .catch(err => console.error(err))
+}
+
+function countFormateurs(key = "") {
+    const url = `${END_POINT}/count/${key}`
+    return axios.get(url)
+        .then(response => response.data["nb"])
+        .catch(err => console.error(err))
+}
+
+function getInterventionsByFormateurId(id, page, size, key = "") {
+    const url = `${END_POINT}/${id}/interventions/${page - 1}/${size}/${key}`
+    return axios.get(url)
+        .then(response => response.data)
+        .catch(err => console.error(err))
+}
+
+function countInterventionsByFormateurId(id, key = "") {
+    const url = `${END_POINT}/${id}/interventions/count/${key}`
+    return axios.get(url)
+        .then(response => response.data["nb"])
+        .catch(err => console.error(err))
+}
