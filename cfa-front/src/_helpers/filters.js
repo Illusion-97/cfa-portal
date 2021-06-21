759febb768e39302dfeac186038875e01f91a470
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-06-21 16:44:32 +0200

    create filter to get full address and fullname

--- cfa-front/src/_helpers/filters.js
+++ cfa-front/src/_helpers/filters.js
@@ -2,18 +2,18 @@
 import Vue from "vue";
 
 Vue.filter("uppercase", function (value) { // les mots sont en majuscule
-    if (!value) return "";
-    value = value.toString();
-    return value.toUpperCase();
+  if (!value) return "";
+  value = value.toString();
+  return value.toUpperCase();
 });
 
 Vue.filter("capitalize", function (value) { // premiere letter en majuscule
-    if (!value) return "";  
-    value = value.toString();
-    return value.charAt(0).toUpperCase() + value.slice(1);
+  if (!value) return "";
+  value = value.toString();
+  return value.charAt(0).toUpperCase() + value.slice(1);
 });
 
-Vue.filter("formatNumber", function(value) { // formate les nombres en son entier inferieur
+Vue.filter("formatNumber", function (value) { // formate les nombres en son entier inferieur
   if (typeof value != "number") {
     value = parseInt(value);
     Math.floor(value);
@@ -21,12 +21,20 @@ Vue.filter("formatNumber", function(value) { // formate les nombres en son entie
   return Math.floor(value);
 });
 
-Vue.filter("twoDigits", function(value) { // arondi le nombre au centième
-  if (typeof value != "number" || !value) 
+Vue.filter("twoDigits", function (value) { // arondi le nombre au centième
+  if (typeof value != "number" || !value)
     return '';
-  
-  let result = value*100;
+
+  let result = value * 100;
   result = Math.round(result);
   result = result / 100;
   return result;
 });
+
+Vue.filter("fullAddresse", (value) => {
+  return `${value.numero} ${value.rue} ${value.ville} ${value.codePostal}`
+})
+
+Vue.filter("fullName", value => {
+  return `${value.nom} ${value.prenom}`
+})
