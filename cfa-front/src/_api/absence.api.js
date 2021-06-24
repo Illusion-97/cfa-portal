22e6d530b0e77f6cb3fc8020df54811cebfa73ed
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-06-24 14:40:50 +0200

    upddate absence service methods

--- cfa-front/src/_api/absence.api.js
+++ cfa-front/src/_api/absence.api.js
@@ -7,26 +7,35 @@ export const absencesApi = {
   getAllAbsences,
   save,
   deleteAbsence,
+  countAbsence,
 };
 
-export async function getAllAbsences() {
-    return axios
-      .get(`${END_POINT}`, requestOptions.headers())
-      .then((response) => response.data)
-      .catch((error) => console.log(error));
-  }
+function getAllAbsences(page, size, key = "") {
+  const url = `${END_POINT}/${page - 1}/${size}/${key}`
+  return axios
+    .get(url, requestOptions.headers())
+    .then((response) => response.data)
+    .catch((error) => console.log(error));
+}
 
 
-export async function save(absence) {
+function save(absence) {
   return axios
     .post(`${END_POINT}`, absence, requestOptions.headers())
     .then((response) => response.data)
     .catch((error) => console.log(error));
 }
 
-export async function deleteAbsence(id) {
+function deleteAbsence(id) {
   return axios
     .delete(`${END_POINT}/${id}`, requestOptions.headers())
     .then((response) => response.data)
     .catch((error) => console.log(error));
+}
+
+function countAbsence(key = "") {
+  const url = `${END_POINT}/count/${key}`
+  return axios.get(url, requestOptions.headers())
+    .then(response => response.data["nb"])
+    .catch(err => console.error(err))
 }
