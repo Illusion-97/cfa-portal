19a760920110ee2336041ad4cc2491ef46bd3ba1
Nicolas <npotier@dawan.fr>
2021-07-29 17:01:15 +0200

    grp list / create / delte / REFERENT

--- cfa-front/src/views/Referent/CreateGroupe.vue
+++ cfa-front/src/views/Referent/CreateGroupe.vue
@@ -1,87 +1,160 @@
 <template>
-  <div id="CreateGroupe">
-    <BodyTitle title="Créer un groupe" />
-    <div class="container">
-      <form>
-        <div class="mb-3">
-          <label for="inputNom" class="form-label">Nom</label>
-          <br/>
-          <input
-            type="text"
-            class="form-control"
-            id="inputNom"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="inputDescription" class="form-label">Decription</label>
-          <br/>
-          <input
-            type="text"
-            class="form-control"
-            id="inputDescription"
-          />
-        </div>
-        <br/>
-         Ajouter des membres au groupe
-        <div class="input-group">
-          <select class="custom-select" id="inputGroupSelect">
-            <option selected>Séléctionner</option>
-            <option value="1">Nicolas</option>
-            <option value="2">Ali</option>
-            <option value="3">Tanguy</option>
-            <option value="4">Kanha</option>
-          </select>
-          <div class="input-group-append">
-            <button v-on:click="ajouter" class="btn btn-primary" type="button">
-              Ajouter
-            </button>
-          </div>
-        </div>
-        <br/>
-        Membres du groupe :
-        <br />
-        <textarea
-          v-model="message"
-          placeholder="Filtrer les membres"
-        ></textarea>
-        <br/>
-        <button v-on:click="create" class="btn btn-primary" type="button">
-              Créer
-            </button>
-      </form>
-    </div>
-    <br>
-    <router-link
-      :to="{ name: 'referent-groupes' }"
+  <div class="container-fluid">
+    <a
+      @click="goBack()"
       class="h5"
       style="cursor:pointer; color:black;text-decoration:none;"
     >
       <font-awesome-icon :icon="['fas', 'chevron-left']" class="icon" />
       Precedent
-    </router-link>
+    </a>
+    
+    <BodyTitle :title="vue_title" />
+
+    <b-form class="form mb-5" @submit="submit">
+      <b-form-group class="mb-5">
+        <b-form-row class="text-align-left">
+          <label class="mon-label">Nom</label>
+          <div class="mon-input">
+            <b-form-input type="text" v-model="form.nom"> </b-form-input>
+          </div>
+        </b-form-row>
+      </b-form-group>
+
+      <b-form-group class="mb-5">
+        <b-form-row class="text-align-left">
+          <label class="mon-label">Etudiants :</label>
+          <a class="btn btn-primary" @click="showModal">Ajouter des étudiant</a>
+        </b-form-row>
+      </b-form-group>
+
+      <table class="table">
+        <thead class="">
+          <tr>
+            <th>Prenom Nom</th>
+            <th>Email</th>
+            <th>Promotions</th>
+          </tr>
+        </thead>
+        <tbody >
+          <tr v-for="etudiant in etudiantsComputed" :key="etudiant.id">
+            <td>{{ etudiant.prenom }} {{ etudiant.nom }}</td>
+            <td>{{ etudiant.login }}</td>
+            <td>
+              <div
+                v-for="promotion in etudiant.promotionsDto"
+                :key="promotion.id"
+              >
+                {{ promotion.nom }}
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div class="offset-10 col-3 pr-5 pl-0">
+        <button type="submit" class="btn btn-primary mon-btn">
+          {{ btn_form_text }}
+        </button>
+      </div>
+    </b-form>
+
+    <EtudiantModal
+      v-show="isModalVisible"
+      @close="closeModal"
+      :etudiantsProp="etudiantsComputed"
+      v-on:close="onClickClose"
+    />
   </div>
 </template>
 
 <script>
 import BodyTitle from "@/components/utils/BodyTitle.vue";
-export default {
-  name: "CreateGroupe",
+import { groupeApi } from "@/_api/groupe.api.js";
 
+import EtudiantModal from "@/components/Modal/EtudiantModal.vue";
+
+export default {
+  name: "GroupeCreate",
   components: {
     BodyTitle,
+    EtudiantModal,
+  },
+  data() {
+    return {
+      vue_title: "Création d'un nouveau groupe",
+      btn_form_text: "Ajouter",
+
+      form: {
+        nom: "",
+        etudiantsDto: [],
+      },
+
+      isModalVisible: false,
+    };
+  },
+  computed: {
+    etudiantsComputed() {
+      return this.form.etudiantsDto;
+    },
+  },
+  methods: {
+    submit(e) {
+      e.preventDefault();
+      groupeApi
+        .save(this.form)
+        .then(() => this.$router.push({ name: "referent-groupes" }));
+    },
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
+    goBack() {
+      this.$router.go(-1);
+    },
+    onClickClose(etudiants){
+      this.form.etudiantsDto = etudiants;
+    }
+  },
+  created() {
+    if (
+      this.$route.params.id != null &&
+      this.$route.params.id != "" &&
+      this.$route.params.id != 0
+    ) {
+      groupeApi.getById(this.$route.params.id).then((response) => {
+        this.form = response;
+        this.vue_title = "Update d'un Groupe";
+        this.btn_form_text = "Update";
+      });
+    }
   },
 };
 </script>
+
 <style scoped>
-.opts,
-label {
-  color: black;
+.header-list {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 0.5%;
 }
-.table {
-  text-align: center;
+
+.header-list > form {
+  width: 40%;
 }
-.icon:hover {
-  cursor: pointer;
+
+.mon-btn {
+  width: 80%;
+}
+
+.mon-label {
+  margin-left: 2.2em;
+  width: 9.7em;
+}
+
+.mon-input {
+  width: 32em;
 }
 </style>
-
--- cfa-front/src/views/Referent/Groupes.vue
+++ cfa-front/src/views/Referent/Groupes.vue
@@ -1,118 +1,21 @@
 <template>
-  <div id="Groupes">
-    <BodyTitle title="Liste des groupes" />
-
-    <div class="mon-group">
-            <label class="form-label"
-              >Choisissez une promotion pour affiner la recherche :
-            </label>
-            <select
-              class="custom-select"
-              v-model="selected"
-              @change="onSelected()"
-            >
-              <option
-                v-for="promotion in promotionsComputed"
-                :key="promotion.id"
-                :value="promotion"
-                >{{ promotion.nom }}</option
-              >
-            </select>
-          </div>
-          <br>
-
-    <div class="container">
-      <router-link class="btn btn-primary" :to="{ name:'referent_create-groupe'}">Créer un groupe</router-link>
-    </div>
-
-
-    <TableTemplate
-      :perPage="perPage"
-      :items="items"
-      :fields="fields"
-      :showBtn="false"
-      btnLink="/formateur/blabla"
-    />
-    <router-link
-      :to="{ name: 'referent_groupe-projet' }"
-      class="h5"
-      style="cursor:pointer; color:black;text-decoration:none;"
-    >
-      <font-awesome-icon :icon="['fas', 'chevron-left']" class="icon" />
-      Precedent
-    </router-link>
+  <div class="container-fluid">
+    <BodyTitle title="Liste des Groupes" />
+    <GroupeListComponent :isAction="true"/>
   </div>
 </template>
 
 <script>
 import BodyTitle from "@/components/utils/BodyTitle.vue";
-import TableTemplate from "@/components/utils/TableTemplate.vue";
-import { groupesFields } from "@/assets/js/fieldsReferent.js"
-import axios from "axios";
-import { requestOptions } from '@/_helpers/request-options.js';
-import { promotionApi } from "@/_api/promotion.api.js";
+import GroupeListComponent from "@/components/Referent/GroupeListComponent.vue";
+
 export default {
-  name: "Groupes",
+  name: "GroupeListe",
   components: {
     BodyTitle,
-    TableTemplate,
+    GroupeListComponent,
   },
-  data() {
-    return {
-      perPage: 10,
-      etudiantsBDD: null,
-      promotions: null,
-
-      selected: null,
-      items: [/*
-        {
-          nom: "Team1",
-        },
-        {
-          nom: "Team2",
-        },
-        {
-          nom: "Team3",
-        },
-      */],
-      fields: groupesFields,
-       
-    };
-  },
-  computed: {
-    promotionsComputed() {
-      return this.promotions;
-    },
-    etudiantsBDDComputed() {
-      return this.etudiantsBDD;
-    },
-  },
-  methods: {
-    onSelected() {
-      promotionApi
-        .getEtudiants(this.selected.id)
-        .then((response) => (this.etudiantsBDD = response));
-    },
-  },
-  created() {
-        axios
-          .get("groupeEtudiants/", requestOptions.headers())
-          .then((response) => (this.items = response.data))
-          .catch((e) => this.errors.push(e));
-          promotionApi.getAll().then((response) => (this.promotions = response));
-        },
 };
 </script>
-<style scoped>
-.opts,
-label {
-  color: black;
-}
-.table {
-  text-align: center;
-}
-.icon:hover {
-  cursor: pointer;
-}
-</style>
 
+<style scoped></style>
