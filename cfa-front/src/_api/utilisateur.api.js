dfda967d60a35652aba8cac2aa66869b36076e97
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-08-06 21:50:39 +0200

    Update utilisateur.api.js

--- cfa-front/src/_api/utilisateur.api.js
+++ cfa-front/src/_api/utilisateur.api.js
@@ -1,13 +1,210 @@
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+101
+102
+103
+104
+105
+106
+107
+108
+109
+110
+111
+112
+113
+114
+115
+116
+117
+118
+119
+120
+121
+122
+123
+124
+125
+126
+127
+128
+129
+130
+131
+132
+133
+134
+135
+136
+137
+138
+139
+140
+141
+142
+143
+144
+145
+146
+147
+148
+149
+150
+151
+152
+153
+154
+155
+156
+157
+158
+159
+160
+161
+162
+163
+164
+165
+166
+167
+168
+169
+170
+171
+172
+173
+174
+175
+176
+177
+178
+179
+180
+181
+182
+183
+184
+185
+186
+187
+188
+189
+190
+191
+192
+193
+194
+195
 import axios from "axios";
-import { requestOptions } from "@/_helpers/request-options.js";
+import {
+  requestOptions
+} from "@/_helpers/request-options.js";
 //import handleResponse from '@/_helpers/handle-response.js';
-
 export const utilisateurApi = {
-  
+
   getById,
   getByLogin,
   getAllByPage,
   getCount,
+  getByRoleByPage,
+  getCountByRole,
   getByIdWithObject,
   getAdresseById,
   getPlanningById,
@@ -20,13 +217,10 @@ export const utilisateurApi = {
   deleteUtilisateur,
   getAllUsersByName,
   save,
-
 };
-
 const END_POINT = "utilisateurs";
 const ADRESSE = "adresse";
 const ENTREPRISE = "entreprise";
-
 /*
 function getById(id) {
     let req =  "utilisateurs/" + id;
@@ -34,47 +228,56 @@ function getById(id) {
         //.then(handleResponse);
 }
 */
-
-function getAllByPage(page, size, search = ""){
+function getAllByPage(page, size, search = "") {
   let req = `/${END_POINT}/${page}/${size}/${search}`;
-
-  return  axios
-      .get(req, requestOptions.headers())
-      .then(response => response.data)
-      .catch((error) => console.log(error));
+  return axios
+    .get(req, requestOptions.headers())
+    .then(response => response.data)
+    .catch((error) => console.log(error));
 }
 
-function getCount(search = ""){
+function getCount(search = "") {
   let req = `/${END_POINT}/count/${search}`;
-
-  return  axios
-      .get(req, requestOptions.headers())
-      .then(response => response.data["nb"])
-      .catch((error) => console.log(error));
+  return axios
+    .get(req, requestOptions.headers())
+    .then(response => response.data["nb"])
+    .catch((error) => console.log(error));
 }
 
-function getById(id){
+function getByRoleByPage(role, page, size, search = "") {
+  let req = `/${END_POINT}/${page}/${size}?role=${role}&search=${search}`;
+  return axios
+    .get(req, requestOptions.headers())
+    .then(response => response.data)
+    .catch((error) => console.log(error));
+}
+
+function getCountByRole(role, search = "") {
+  let req = `/${END_POINT}/count?role=${role}&search=${search}`;
+  return axios
+    .get(req, requestOptions.headers())
+    .then(response => response.data["nb"])
+    .catch((error) => console.log(error));
+}
+
+function getById(id) {
   let req = `/utilisateurs/${id}`;
-
-  return  axios
-      .get(req, requestOptions.headers())
-      .then(response => response.data)
-      .catch((error) => console.log(error));
+  return axios
+    .get(req, requestOptions.headers())
+    .then(response => response.data)
+    .catch((error) => console.log(error));
 }
 
-function getByLogin(login){
+function getByLogin(login) {
   let req = `/utilisateurs/email=${login}`;
-
-  return  axios
-      .get(req, requestOptions.headers())
-      .then(response => response.data)
-      .catch((error) => console.log(error));
+  return axios
+    .get(req, requestOptions.headers())
+    .then(response => response.data)
+    .catch((error) => console.log(error));
 }
-
 
 function getByIdWithObject(id) {
   let req = "utilisateurs/" + id + "/with-object";
-
   return axios
     .get(req, requestOptions.headers())
     .then((response) => response.data)
@@ -83,7 +286,6 @@ function getByIdWithObject(id) {
 
 function getAdresseById(id) {
   let req = "utilisateurs/" + id + "/adresse";
-
   return axios
     .get(req, requestOptions.headers())
     .then((response) => response.data)
@@ -92,7 +294,6 @@ function getAdresseById(id) {
 
 function getPlanningById(id) {
   let req = "utilisateurs/" + id + "/planning";
-
   return axios
     .get(req, requestOptions.headers())
     .then((response) => response.data)
@@ -101,13 +302,11 @@ function getPlanningById(id) {
 
 function getRoleById(id) {
   let req = "utilisateurs/" + id + "/roles";
-
   return axios
     .get(req, requestOptions.headers())
     .then((response) => response.data)
     .catch((error) => console.log(error));
 }
-
 
 function getAllUtilisateurs() {
   return axios
@@ -115,16 +314,12 @@ function getAllUtilisateurs() {
     .then((response) => response.data)
     .catch((error) => console.log(error));
 }
-
-
-  async function getAllUsersByName(name) {
-    let users = [];
-    const response = await axios.get(`$/admin/userList?name=${name}`, requestOptions.headers());
-    users = response.data;
-    return users;
-  }
-
-
+async function getAllUsersByName(name) {
+  let users = [];
+  const response = await axios.get(`$/admin/userList?name=${name}`, requestOptions.headers());
+  users = response.data;
+  return users;
+}
 
 function getAllUtilisateursByEntreprise() {
   return axios
@@ -155,15 +350,12 @@ function updateUtilisateur(Utilisateur) {
 }
 
 function save(form) {
-  let req =  "utilisateurs";
-
+  let req = "utilisateurs";
   return axios
-      .post(req, form, requestOptions.headers())
-      .then((response) => response)
-      .catch((error) => console.log(error));
-
+    .post(req, form, requestOptions.headers())
+    .then((response) => response)
+    .catch((error) => console.log(error));
 }
-
 
 function deleteUtilisateur(id) {
   return axios
@@ -171,4 +363,3 @@ function deleteUtilisateur(id) {
     .then((response) => response.data)
     .catch((error) => console.log(error));
 }
-
