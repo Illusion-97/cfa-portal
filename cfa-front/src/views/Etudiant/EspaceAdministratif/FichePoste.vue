dd2123d89862d602b599608aed4d3c3cadb368ec
Nicolas <npotier@dawan.fr>
2021-08-31 12:05:03 +0200

    fiche salarié / poste / entreprise

--- cfa-front/src/views/Etudiant/EspaceAdministratif/FichePoste.vue
+++ cfa-front/src/views/Etudiant/EspaceAdministratif/FichePoste.vue
@@ -2,6 +2,78 @@
     <div>
         <router-link class="router-link" :to="{name:'etudiant_profil'}"> <font-awesome-icon :icon="['fas', 'angle-left']" />  Retour au Profil</router-link>
         <BodyTitle title="Fiche de Poste" />
+
+         <b-card no-body id="card-profil">
+        <b-card-header>
+          <span class="">Identification du poste</span>
+          
+        </b-card-header>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">Intitulé du poste :</span>
+          <span class="col-md-10"> Concepteur / développeur d'application </span>
+        </b-card-text>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">Nature du poste : </span>
+          <span class="col-md-10">********* </span>
+        </b-card-text>
+      </b-card>
+
+        <br>
+
+       <b-card no-body id="card-profil">
+        <b-card-header>
+          <span class="">Identite de l'agent</span>
+          
+        </b-card-header>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">Nom - Prenom :</span>
+          <span class="col-md-10"> Billon Tanguy </span>
+        </b-card-text>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">Status,corps, Catégorie, grade : </span>
+          <span class="col-md-10">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta excepturi officiis pariatur dolorum, magnam quisquam voluptatibus, molestias totam natus rerum labore? Dignissimos alias itaque repudiandae rem possimus nihil laboriosam harum? </span>
+        </b-card-text>
+      </b-card>
+
+        <br>
+       <b-card no-body id="card-profil">
+        <b-card-header>
+          <span class="">Présentation du service</span>
+          
+        </b-card-header>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">Mission principale du service :</span>
+          <span class="col-md-10"> Dév Appli </span>
+        </b-card-text>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">Composition du service : </span>
+          <span class="col-md-10">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta excepturi officiis pariatur dolorum, magnam quisquam voluptatibus, molestias totam natus rerum labore? Dignissimos alias itaque repudiandae rem possimus nihil laboriosam harum? </span>
+        </b-card-text>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">Positionnement de l'agent dans le service : </span>
+          <span class="col-md-10">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta excepturi officiis pariatur dolorum, magnam quisquam voluptatibus, molestias totam natus rerum labore? Dignissimos alias itaque repudiandae rem possimus nihil laboriosam harum? </span>
+        </b-card-text>
+      </b-card>
+        <br>
+       <b-card no-body id="card-profil">
+        <b-card-header>
+          <span class="">Les missions du poste</span>
+          
+        </b-card-header>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">Mission principale, Raison d'être ou finalité du poste :</span>
+          <span class="col-md-10"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi officia, ab quos molestiae fugit, dignissimos nihil nemo atque hic animi odit? Ratione, at ullam. Architecto vel vitae soluta maxime reiciendis. </span>
+        </b-card-text>
+
+      </b-card>
     </div>
 </template>
 
--- cfa-front/src/views/Etudiant/EspaceAdministratif/FicheSalarie.vue
+++ cfa-front/src/views/Etudiant/EspaceAdministratif/FicheSalarie.vue
@@ -2,6 +2,107 @@
     <div>
         <router-link class="router-link" :to="{name:'etudiant_profil'}"> <font-awesome-icon :icon="['fas', 'angle-left']" />  Retour au Profil</router-link>
         <BodyTitle title="Fiche de Salarié" />
+
+        <b-card no-body id="card-profil">
+        <b-card-header>
+          <span class="">Etat Civil</span>
+          
+        </b-card-header>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">Nom :</span>
+          <span class="col-md-4"> prenom </span>
+          <span class="font-weight-bold col-md-2">Prenom : </span>
+          <span class="col-md-4">nom </span>
+        </b-card-text>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">Civilité :</span>
+          <span class="col-md-4"> Monsieur </span>
+          <span class="font-weight-bold col-md-2">Nom de naissance : </span>
+          <span class="col-md-4"> ****** </span>
+        </b-card-text>
+
+        <b-card-text class="row ml-5">
+          <span class="font-weight-bold col-md-2">Adresse : </span>
+          <span class="col-md-10">numero rue </span>
+        </b-card-text>
+
+        <b-card-text class="row ml-5">
+          <span class="font-weight-bold col-md-2">Ville : </span>
+          <span class="col-md-4">ville</span>
+          <span class="font-weight-bold col-md-2">Code Postal : </span>
+          <span class="col-md-4">codePostal</span>
+        </b-card-text>
+
+        <b-card-text class="row ml-5">
+          <span class="font-weight-bold col-md-2">Pays : </span>
+          <span class="col-md-4">France</span>
+          <span class="font-weight-bold col-md-2">Nationalité : </span>
+          <span class="col-md-4">Française</span>
+        </b-card-text>
+
+        <b-card-text class="row ml-5">
+          <span class="font-weight-bold col-md-2">Naissance le : </span>
+          <span class="col-md-4">00/00/0000 à *******</span>
+          <span class="font-weight-bold col-md-2">Dépt : </span>
+          <span class="col-md-4">00</span>
+        </b-card-text>
+
+        <b-card-text class="row ml-5">
+          <span class="font-weight-bold col-md-2">Email : </span>
+          <span class="col-md-4">blabla@dawan.fr</span> 
+          <span class="font-weight-bold col-md-2">Téléphone : </span>
+          <span class="col-md-4">00.00.00.00.00</span>
+        </b-card-text>
+      </b-card>
+
+        <br>
+
+      <b-card no-body id="card-profil">
+        <b-card-header>
+          <span class="">Données salarié</span>
+          
+        </b-card-header>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">Date entrée :</span>
+          <span class="col-md-4"> 00/00/0000 </span>
+          <span class="font-weight-bold col-md-2">Date anciennetée : </span>
+          <span class="col-md-4">00/00/0000 </span>
+        </b-card-text>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">IBAN :</span>
+          <span class="col-md-4"> FR** **** **** **** **** **** *** </span>
+          <span class="font-weight-bold col-md-2">BIC : </span>
+          <span class="col-md-4"> ****** </span>
+        </b-card-text>
+      </b-card>
+
+      
+        <br>
+
+      <b-card no-body id="card-profil">
+        <b-card-header>
+          <span class="">Dernier contrat</span>
+          
+        </b-card-header>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">Profession :</span>
+          <span class="col-md-4"> ** </span>
+          <span class="font-weight-bold col-md-2">Type de contrat : </span>
+          <span class="col-md-4">CDD </span>
+        </b-card-text>
+
+        <b-card-text class="identity row ml-5">
+          <span class="font-weight-bold col-md-2">Du :</span>
+          <span class="col-md-10"> 00/00/0000 au 00/00/0000 </span>
+        </b-card-text>
+
+        
+      </b-card>
     </div>
 </template>
 
