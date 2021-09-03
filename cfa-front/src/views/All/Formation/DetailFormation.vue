c2f5139d32806b59b101da915a4acd1cabc7a932
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-09-03 16:24:26 +0200

    Update DetailFormation.vue

--- cfa-front/src/views/All/Formation/DetailFormation.vue
+++ cfa-front/src/views/All/Formation/DetailFormation.vue
@@ -1,141 +1,95 @@
 <template>
-  <div>
-    <span @click="goBack" class="h5 icon-link d-block">
-      <font-awesome-icon :icon="['fas', 'chevron-left']" />
-      Precedent
-    </span>
-    <div id="grid-container">
-      <div class="card" id="card-detail">
-        <div class="card-header d-flex justify-content-between">
-          <h2>{{item.titre}}</h2>
-          <div class="dropstart">
-            <a class="" href="#" id="navbardrop" data-toggle="dropdown">
-              <font-awesome-icon :icon="['fas', 'ellipsis-v']" class="icon-link text-dark" />
-            </a>
-            <div class="dropdown-menu rounded-0">
-              <router-link :to="{
-                  name: 'modifier-formation',
-                  params: { id: formationId },
-                }" class="icon-link dropdown-item">
-                Modifier
-              </router-link>
-              <span v-on:click="deleteFormation(formationId)" class="icon-link dropdown-item">
-                Supprimer
-              </span>
+  <section>
+    <div id="bg-detail">
+      <span @click="goBack" class="h5 icon-link text-light">
+        <font-awesome-icon :icon="['fas', 'chevron-left']" />
+        Precedent
+      </span>
+      <div class="float-right mr-2" style="font-size:20px">
+        <a class="" href="#" id="navbardrop" data-toggle="dropdown">
+          <font-awesome-icon :icon="['fas', 'sliders-h']" class="icon text-light" />
+        </a>
+        <div class="dropdown-menu dropleft rounded-0">
+          <router-link :to="{name: 'modifier-formation',
+              params: { id: formationId },}" class="icon-link dropdown-item">
+            Modifier
+          </router-link>
+          <span v-on:click="deleteFormation(formationId)" class="icon-link dropdown-item">
+            Supprimer
+          </span>
+          <div class="dropdown-divider"></div>
+          <router-link :to="{ name: 'ajouter-intervention'}" class="icon-link dropdown-item">
+            Ajouter une intervention
+          </router-link>
+        </div>
+      </div>
+      <h1 class="">{{ item.titre }}</h1>
+      <div id="grid-container">
+        <div id="card-detail">
+          <div class="card card-info">
+            <p>
+              <span class="font-weight-bold">Intitulé de la formation: </span>
+              {{ item.titre }}
+            </p>
+            <p>
+              <span class="font-weight-bold">Description :</span>
+              {{ item.contenu }}
+            </p>
+            <p><span class="font-weight-bold">Cursus associé: </span>
+              <ul v-if="item.cursusLstDto.length > 0">
+                <li v-for="cursus in item.cursusLstDto" :key="cursus.id">
+                  {{cursus.titre}}
+                </li>
+              </ul>
+              <span v-else>Aucun cursus</span>
+            </p>
+          </div>
+        </div>
+        <div id="card-intervention">
+          <div class="card card-info-intervention">
+            <div class="card-header">
+              Prochaines sessions d'intervention
             </div>
-          </div>
-          <!-- <h2>{{ items.formationDto.titre }}</h2> -->
-        </div>
-        <div class="card-body">
-          <table class="table table-bordered table-hover">
-            <tbody>
-              <tr>
-                <th>Intitulé de la formation</th>
-                <td>{{ item.titre }}</td>
-              </tr>
-              <tr>
-                <th>Description</th>
-                <td>{{item.contenu}}</td>
-                <!-- <td v-if="trainers.length > 0">
-                  <ul class="list-style-none" v-for="t in trainers" :key="t.id">
-                    <li>{{t.nom}} {{t.prenom}}</li>
-                  </ul>
-                </td>
-                <td v-else>
-                  <span>Aucun formateurs affecté</span>
-                </td> -->
-              </tr>
-              <tr>
-                <th>Cursus</th>
-                <td>
-                  <ul class="list-style-none">
-                    <span v-for="cursus in item.cursusLstDto" :key="cursus.id">{{cursus.titre}}</span>
-                    <span v-if="item.cursusLstDto.length == 0">Aucun cursus</span>
-                  </ul>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div id="intervention-list">
-        <div class="card intervention-card">
-          <div class="card-header">
-            <h3>Interventions</h3>
-          </div>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab perferendis corrupti consequatur, cumque eos, est ullam quas neque nulla dolorem totam! Distinctio maxime optio et, sit illum molestiae. Fugiat, est!
-        </div>
-      </div>
-      <!-- <div id="student-list">
-        <span class="btn-toggle" data-toggle="collapse" href="#Collapse1" role="button" aria-expanded="false"
-          aria-controls="Collapse1">Afficher les étudiants</span>
-        <div class="collapse" id="Collapse1">
-          <div class="card card-body border-0"> -->
-            <!-- <table class="table text-center table-sm">
-              <thead>
-                <tr>
-                  <th scope="col">Nom</th>
-                  <th scope="col">Prenom</th>
-                  <th scope="col">Email</th>
-                </tr>
-              </thead>
+            <small class="form-text info-text ml-1">
+              <font-awesome-icon :icon="['fas', 'info-circle']" />
+                Double-cliquez sur une session pour plus d'info.
+            </small>
+            <table class="table text-center">
               <tbody>
-                <tr v-for="stud in students" :key="stud.id">
-                  <td>{{stud.nom}}</td>
-                  <td>{{stud.prenom}}</td>
-                  <td>{{stud.login}}</td>
+                <tr>
+                  <th>Date de debut</th>
+                  <th>Date de fin</th>
+                </tr>
+                <tr v-for="itv in interventions" :key="itv.id" @dblclick="getDetail(itv.id)">
+                  <td>{{ itv.dateDebut | formatDate }}</td>
+                  <td>{{ itv.dateFin | formatDate }}</td>
                 </tr>
               </tbody>
-            </table> -->
-          <!-- </div>
+            </table>
+          </div>
         </div>
-      </div> -->
-
-      <!-- <div id="promotion-list">
-        <span class="btn-toggle" data-toggle="collapse" href="#Collapse2" role="button" aria-expanded="false"
-          aria-controls="Collapse2">Afficher les promotions</span>
-        <div class="collapse show" id="Collapse2">
-          <div class="card card-body border-0"> -->
-            <!-- <ul v-for="p in promo" :key="p.id" class="list-style-none">
-              <li>{{ p.nom }}</li>
-            </ul> -->
-            <!-- <pre>{{ promo }}</pre> -->
-          <!-- </div>
-        </div>
-      </div> -->
-      <!-- <div id="devoir-list">
-        <span class="btn-toggle" data-toggle="collapse" href="#Collapse3" role="button" aria-expanded="false"
-          aria-controls="Collapse3">Devoirs</span>
-        <div class="collapse" id="Collapse3">
-          <div class="card card-body border-0"> -->
-            <!-- <span v-if="assignements.length > 0">
-              <pre>{{assignements}}</pre>
-            </span>
-            <span v-else class="text-center">
-              Aucun devoirs
-            </span> -->
-          <!-- </div>
-        </div>
-      </div> -->
+      </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
   import { formationApi } from "@/_api/formation.api.js";
   export default {
-    name: "DetailIntervention",
+    name: "DetailFormation",
     data() {
       return {
         formationId: this.$route.params.id,
         item: {
-          cursusLstDto: {}
+          cursusLstDto: {},
         },
+        interventions: [],
         loading: false,
       };
     },
     created() {
       this.getId();
+      this.getInterventionsByFormationId();
     },
     methods: {
       goBack() {
@@ -144,16 +98,27 @@
       getId() {
         formationApi
           .getFormationById(this.formationId)
-          .then((data) => {
-            this.item = data;
-          });
+          .then((data) => (this.item = data));
+      },
+      getInterventionsByFormationId() {
+        formationApi
+          .getInterventionsByFormationId(this.formationId)
+          .then((data) => (this.interventions = data));
       },
       deleteFormation(id) {
-        formationApi.deleteFormation(id).then(res => {
+        formationApi.deleteFormation(id).then((res) => {
           if (res.status == 202)
             this.$router.push({
-              name: 'all-formation'
-            })
+              name: "all-formation",
+            });
+        });
+      },
+      getDetail(id) {
+        this.$router.push({
+          name: "intervention-detail",
+          params: {
+            id: id
+          }
         })
       }
     },
@@ -162,6 +127,25 @@
 </script>
 
 <style scoped>
+  h1 {
+    color: #fff;
+    position: relative;
+    top: 25%;
+    text-align: center;
+  }
+
+  #bg-detail {
+    background-image: linear-gradient(rgba(22, 22, 22, 0.877),
+        rgba(116, 116, 116, 0.294)),
+      url("~@/assets/img/books.jpg");
+    /* background-size: 100vh; */
+    background-position: bottom;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    height: 30vh;
+    padding: 0.5em;
+  }
+
   .icon-link {
     cursor: pointer;
     color: black;
@@ -171,42 +155,49 @@
 
   #grid-container {
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(2,1fr);
+    grid-template-columns: repeat(4, 1fr);
+    /* grid-template-rows: repeat(2, 1fr); */
     gap: 1em;
+    position: relative;
+    top: 8em;
   }
 
   #card-detail {
-    grid-column: 2;
+    grid-column: 1 / span 3;
     grid-row: 1;
   }
-  
-  #intervention-list {
-    grid-column: 1/ span 3;
+
+  .card-info {
+    margin-left: 1em;
+    padding: 0.5em;
+    /* margin: 0 auto; */
   }
 
-.intervention-card {
-  width: 70%;
-  margin: 0 auto;
-}
-  /* #student-list {
-    grid-row: 2;
-    grid-column: 1;
+  #card-intervention {
+    grid-column: 4;
   }
 
-  #promotion-list {
-    grid-row: 2;
-    grid-column: 2;
+  .card-info-intervention {
+    margin-right: 1em;
   }
-
-  #devoir-list {
-    grid-row: 2;
-    grid-column: 3;
-  } */
 
   .table th {
     border-top: 0;
   }
+
+  .table tr {
+    cursor: pointer;
+  }
+
+  .table tr:hover:first-child {
+    background-color: inherit;
+    cursor: auto;
+  }
+
+  .table tr:hover {
+    background-color: rgba(160, 158, 158, 0.144);
+  }
+
 
   .list-style-none {
     list-style: none;
@@ -214,7 +205,7 @@
     padding: 0;
   }
 
-  .btn-toggle {
+  /* .btn-toggle {
     border-radius: 0;
     position: relative;
     display: flex;
@@ -229,5 +220,5 @@
     height: 1px;
     width: 30%;
     border-bottom: 1px solid black;
-  }
+  } */
 </style>
