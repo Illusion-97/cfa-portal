04a2e5bab1c44add9906f0e84e75cbc16a32f860
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-09-03 16:23:40 +0200

    remove bootstrap bundle

--- cfa-front/src/plugins/bootstrap-vue.js
+++ cfa-front/src/plugins/bootstrap-vue.js
@@ -3,6 +3,6 @@ import Vue from 'vue'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
