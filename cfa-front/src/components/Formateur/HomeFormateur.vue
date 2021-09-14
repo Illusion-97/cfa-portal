230a355282ba5264d72b53b0ef4a466dde294cc0
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-09-14 16:41:05 +0200

    Update HomeFormateur.vue

--- cfa-front/src/components/Formateur/HomeFormateur.vue
+++ cfa-front/src/components/Formateur/HomeFormateur.vue
@@ -1,19 +1,36 @@
 <template>
-    <section>
-        <Planning/>
-    </section>
+  <section>
+    <div id="grid-container">
+      <div id="header"></div>
+      <div id="trainer-planning">
+        <Planning />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import Planning from '@/components/utils/Planning.vue'
-    export default {
-        name:'HomeFormateur',
-        components:{
-            Planning
-        }
-    }
+import Planning from "@/components/utils/Planning.vue";
+export default {
+  name: "HomeFormateur",
+  components: {
+    Planning,
+  },
+};
 </script>
 
 <style lang="css" scoped>
-
+#grid-container {
+  display: grid;
+  grid-template-rows: 55vh 1fr;
+  row-gap: 2em;
+}
+#header{
+    grid-row: 1;
+    background-color: #f5f5f5;
+}
+#trainer-planning {
+    grid-row: 2;
+    margin-bottom: 2em;
+}
 </style>
