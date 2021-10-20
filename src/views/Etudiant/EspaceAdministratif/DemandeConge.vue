f86814787c163c8197605ff0e025d99920267676
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-10-20 14:36:22 +0200

    add notification on successful submit form

--- src/views/Etudiant/EspaceAdministratif/DemandeConge.vue
+++ src/views/Etudiant/EspaceAdministratif/DemandeConge.vue
@@ -17,16 +17,26 @@
       </tbody>
     </table>
 
-    <form class="mb-5" @submit="submit">
+    <form @submit="submit" class="my-5">
       <div class="d-flex justify-content-center form-group">
         <div class="col-4">
           <label for="">Date de debut</label>
-          <b-form-datepicker locale="fr" v-model="form.dateDebut" placeholder="Aucune date selectionné" required>
+          <b-form-datepicker
+            locale="fr"
+            v-model="form.dateDebut"
+            placeholder="Aucune date selectionné"
+            required
+          >
           </b-form-datepicker>
         </div>
         <div class="col-4">
           <label for="">Date de fin</label>
-          <b-form-datepicker locale="fr" v-model="form.dateFin" placeholder="Aucune date selectionné" required>
+          <b-form-datepicker
+            locale="fr"
+            v-model="form.dateFin"
+            placeholder="Aucune date selectionné"
+            required
+          >
           </b-form-datepicker>
         </div>
       </div>
@@ -48,105 +58,141 @@
             v-on:change="handleFileUpload()"
             accept="application/pdf"
           />
-   
-          </div>
-      </div>
-      
-      <div class="d-flex justify-content-center form-group">
-        <div class="col-8">
-        <textarea name="motif" cols="20" rows="3" class="form-control" v-model="form.motif" placeholder="Motif du congé"/>
         </div>
       </div>
-      <input type="submit" value="Envoyer" class="btn btn-outline-primary px-5 float-right">
+
+      <div class="d-flex justify-content-center form-group">
+        <div class="col-8">
+          <textarea
+            name="motif"
+            cols="20"
+            rows="3"
+            class="form-control"
+            v-model="form.motif"
+            placeholder="Motif du congé"
+          />
+        </div>
+      </div>
+      <div class="form-row form-group">
+        <input
+          type="submit"
+          value="Envoyer"
+          class="btn btn-outline-primary mx-auto"
+          style="width:30em"
+        />
+      </div>
     </form>
-    <!-- {{conges}} -->
-    <!-- <pre class="m-0">
-      {{form}}
-    </pre> -->
-    <TableTemplate :perPage="perPage" :items="congesComputed" :fields="fields" :isPaginate="false" :current-page="0"/>
+    <div class="alert alert-success" role="alert" v-if="successAlert == true" style="position:absolute;top:1em;right:1em">
+      Demande de congé envoyé
+    </div>
+    <!-- <div class="alert alert-danger" role="alert" v-if="errorAlert == true" style="position:absolute;top:1em;right:1em">
+      Erreur
+    </div> -->
+    <TableTemplate
+      :perPage="perPage"
+      :items="congesComputed"
+      :fields="fields"
+      :isPaginate="false"
+      :current-page="0"
+      :showSearchBar="false"
+    />
   </div>
 </template>
 
 <script>
-  import {  congeApi } from "@/_api/conge.api.js";
-  import { fileApi } from "@/_api/file.api.js";
-  import BodyTitle from "@/components/utils/BodyTitle.vue";
-  import TableTemplate from "@/components/utils/TableTemplate.vue";
-  import { leaveFields } from "@/assets/js/fields.js";
-  export default {
-    name: "DemandeConge",
-    components: {
-      BodyTitle,
-      TableTemplate,
-    },
-    data() {
-      return {
-        file: "",
-        form: {
-          dateDebut: "",
-          dateFin: "",
-          motif: "",
-          type: "",
-          status: "EN_ATTENTE",
-          utilisateurDto: this.$store.getters.getUtilisateur,
-          justificatif: "",
+import { congeApi } from "@/_api/conge.api.js";
+import { fileApi } from "@/_api/file.api.js";
+import BodyTitle from "@/components/utils/BodyTitle.vue";
+import TableTemplate from "@/components/utils/TableTemplate.vue";
+import { leaveFields } from "@/assets/js/fields.js";
+export default {
+  name: "DemandeConge",
+  components: {
+    BodyTitle,
+    TableTemplate,
+  },
+  data() {
+    return {
+      file: "",
+      form: {
+        dateDebut: "",
+        dateFin: "",
+        motif: "",
+        type: "",
+        status: "EN_ATTENTE",
+        utilisateurDto: this.$store.getters.getUtilisateur,
+        justificatif: "",
+      },
+
+      types: [
+        {
+          text: "Maladie",
+          value: "MALADIE",
         },
+        {
+          text: "Payé",
+          value: "PAYE",
+        },
+        {
+          text: "Sans solde",
+          value: "SANS_SOLDE",
+        },
+      ],
 
-        types: [{
-            text: "Maladie",
-            value: "MALADIE"
-          },
-          {
-            text: "Payé",
-            value: "PAYE"
-          },
-          {
-            text: "Sans solde",
-            value: "SANS_SOLDE"
-          },
-        ],
-
-        conges: [],
-        fields: leaveFields,
-        perPage: 10,
-
-        tableConge: [],
-      };
+      conges: [],
+      fields: leaveFields,
+      perPage: 20,
+      successAlert: false,
+      // errorAlert: false,
+      tableConge: [],
+    };
+  },
+  computed: {
+    utilisateur() {
+      return this.$store.getters.getUtilisateur;
     },
-    computed: {
-      utilisateur() {
-        return this.$store.getters.getUtilisateur;
-      },
-      congesComputed() {
-        return this.conges;
-      },
+    congesComputed() {
+      return this.conges;
     },
-    created() {
+  },
+  created() {
+    congeApi
+      .getCongesByUtilisateurId(this.utilisateur.id)
+      .then((response) => (this.conges = response));
+    congeApi
+      .getTableConge(this.utilisateur.id)
+      .then((response) => (this.tableConge = response));
+  },
+  methods: {
+    handleFileUpload() {
+      this.file = this.$refs.file.files[0];
+    },
+    submit(e) {
+      e.preventDefault();
+
+      this.form.justificatif = this.file.name;
+
+      //TODO : save du file
+
       congeApi
-        .getCongesByUtilisateurId(this.utilisateur.id)
-        .then((response) => (this.conges = response));
-      congeApi
-        .getTableConge(this.utilisateur.id)
-        .then((response) => (this.tableConge = response));
-    },
-    methods: {
-      handleFileUpload() {
-        this.file = this.$refs.file.files[0];
-      },
-      submit(e) {
-        e.preventDefault();
-
-        this.form.justificatif = this.file.name;
-
-        //TODO : save du file
-
-        congeApi
-          .save(this.form)
-          //Quand on a ajouté le congé, on recharge la liste
-          .then(() =>
-            congeApi
+        .save(this.form)
+        .then((response) => {
+          if (response.status == 200) { 
+            //affiche une alerte si le formulaire est bien envoyé
+            this.successAlert = true;
+            setTimeout(() => this.successAlert = false,2000)
+          }
+          // else {
+          //   this.errorAlert = true
+          // }
+        })
+        //Quand on a ajouté le congé, on recharge la liste
+        .then(() =>
+          congeApi
             .getCongesByUtilisateurId(this.utilisateur.id)
-            .then((response) => (this.conges = response))
+            .then((response) => {
+              this.conges = response;
+            })
             //Si pas d'erreur, on post le fichier
             .then(() => {
               if (this.file != "")
@@ -156,8 +202,8 @@
                   this.file
                 );
             })
-          );
-      },
+        );
     },
-  };
+  },
+};
 </script>
