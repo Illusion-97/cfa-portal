9bb8f1c7a189ba099b5223ec8e02de642f2c8de6
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-11-03 14:00:49 +0100

    user detail

--- src/views/Admin/Crud/User/DetailUser.vue
+++ src/views/Admin/Crud/User/DetailUser.vue
@@ -9,7 +9,7 @@
       Precedent
     </a>
 
-    <b-card no-body id="my-card">
+    <!-- <b-card no-body id="my-card">
         <b-card-header>
           <span class="">Details</span>
           <div class="float-right mr-2" style="font-size:20px">
@@ -31,8 +31,6 @@
         </b-card-header>
 
         <b-card-text class="identity row ml-5">
-          <span class="font-weight-bold col-md-2">Id :</span>
-          <span class="col-md-4">{{ user.id }}</span>
           <span class="font-weight-bold col-md-2">Prénom : </span>
           <span class="col-md-4">{{ user.prenom }}</span>
         </b-card-text>
@@ -57,15 +55,51 @@
 
 
         <b-card-text class="identity row ml-5">
-          <!-- <span class="font-weight-bold col-md-2">Mot de passe :</span>
-          <span class="col-md-4">{{ user.password }} </span> -->
           <span class="font-weight-bold col-md-2">Rôle :</span>
           <span class="col-md-10"><p v-for="role in user.rolesDto" :key="role.id">{{ role.intitule }}</p> </span>
         </b-card-text>
 
        
-    </b-card>
-
+    </b-card> -->
+    <div class="card mb-3 custom-card">
+      <div class="row no-gutters">
+        <div class="col-md-4 text-center identity">
+          <span class="h1">{{ user.nom | uppercase }}</span>
+          <span class="h2">{{ user.prenom | capitalize }}</span>
+        </div>
+        <div class="col-md-8">
+          <div class="float-right dropleft">
+            <span
+              class=""
+              id="navbardrop"
+              data-toggle="dropdown"
+              style="cursor:pointer;"
+            >
+              <b-icon icon="gear" />
+            </span>
+            <div class="dropdown-menu p-0 rounded-0">
+              <span @click="updateUser()" class="icon-link dropdown-item">
+                Modifier
+              </span>
+              <span class="dropdown-item" @click="deleteUser()">Supprimer</span>
+            </div>
+          </div>
+          <div class="card-body user-info">
+            <p>
+              <span class="font-weight-bold mr-2">&#64;</span>: {{ user.login }}
+            </p>
+            <p>
+              <span class="mr-2"> <b-icon icon="geo-alt-fill" /> </span>:
+              {{ adresseDto | fullAddresse }}
+            </p>
+            <p>
+              <span class="mr-2"> <b-icon icon="telephone-fill"/></span>:
+              {{ user.telephone }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -73,8 +107,7 @@
 import { utilisateurApi } from "@/_api/utilisateur.api.js";
 export default {
   name: "UserDetail",
-  components: {
-  },
+  components: {},
   data() {
     return {
       userId: this.$route.params.id,
@@ -88,7 +121,7 @@ export default {
     goBack() {
       this.$router.go(-1);
     },
-     updateUser() {
+    updateUser() {
       let route = this.$route.path.split("/").splice(1);
       if (route[0] == "admin") {
         this.$router.push({
@@ -109,17 +142,22 @@ export default {
       }
     },
     deleteUser() {
-      utilisateurApi.deleteUtilisateur(this.$route.params.id).then(() => this.goBack());
+      utilisateurApi
+        .deleteUtilisateur(this.$route.params.id)
+        .then(() => this.goBack());
     },
   },
   created() {
-    utilisateurApi.getById(this.$route.params.id).then(response => {this.user = response; this.adresseDto = this.user.adresseDto;});
+    utilisateurApi.getById(this.$route.params.id).then((response) => {
+      this.user = response;
+      this.adresseDto = this.user.adresseDto;
+    });
   },
 };
 </script>
 
 <style scoped>
-#my-card {
+/* #my-card {
   width: 90%;
   padding-bottom: 1em;
   margin-bottom: 5em;
@@ -147,5 +185,59 @@ export default {
   display: inline-block;
   width: 40em;
   padding-left: 3em;
+} */
+
+.a {
+  outline: none;
+  text-decoration: none;
+}
+
+#dawan-logo {
+  width: 15em;
+  float: left;
+}
+
+#iconde-rewrite {
+  width: 2em;
+}
+
+.mon-container {
+  margin-bottom: 5em;
+}
+
+.custom-card {
+  width: 80%;
+  margin: 0 auto;
+  padding: 1em;
+  border-radius: 0;
+}
+
+.identity {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  border-right: 1px solid rgb(95, 95, 95);
+}
+
+.user-info {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.indentity {
+  display: inline;
+}
+
+.link {
+  width: 1em;
+  color: black;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-item {
+  cursor: pointer;
 }
 </style>
