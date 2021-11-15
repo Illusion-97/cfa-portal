033497b2db66c47789f87665c1b919a1d2b02397
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-11-15 17:56:01 +0100

    redirection function if admin or cef

--- src/store/modules/utilisateur.module.js
+++ src/store/modules/utilisateur.module.js
@@ -1,4 +1,5 @@
 import { utilisateurApi } from "@/_api/utilisateur.api.js";
+import router from "@/router/router.js";
 
 export const utilisateur = {
     state: {
@@ -8,10 +9,10 @@ export const utilisateur = {
             password: null,
             prenom: null,
             nom: null,
-            civilite:null,
-            dateDeNaissance:null,
-            telephone:null,
-            adresseDto:null,
+            civilite: null,
+            dateDeNaissance: null,
+            telephone: null,
+            adresseDto: null,
             entrepriseDto: null,
             rolesDto: [],
             etudiantDto: null,
@@ -25,20 +26,36 @@ export const utilisateur = {
         },
     },
     mutations: {
-        SET_UTILISATEUR( state, data){
+        SET_UTILISATEUR(state, data) {
             state.utilisateur = data;
 
             //Si referent, on lui ajoute le role
-            utilisateurApi.isReferent(data.id).then( response => {
-                if(response) state.utilisateur.rolesDto.push({id: 999, intitule: "REFERENT"});
-                
+            utilisateurApi.isReferent(data.id).then(response => {
+                if (response) state.utilisateur.rolesDto.push({ id: 999, intitule: "REFERENT" });
+
+            })
+
+            utilisateurApi.getByLogin(state.utilisateur.login).then(res => {
+                for (let i = 0; i < res.rolesDto.length; i++) {
+                    switch (res.rolesDto[i].intitule) {
+                        case "ADMIN":
+                            router.push({ name: "admin_dashboard" });
+                            break;
+                        case "CEF":
+                            router.push({ name: "cef_dashboard" });
+                            break;
+                        default:
+                            router.push("/");
+                            break;
+                    }
+                }
             })
         },
     },
     actions: {
-        setUtilisateur({ commit }, data){
+        setUtilisateur({ commit }, data) {
             commit('SET_UTILISATEUR', data);
-          }
+        }
     }
 }
 
