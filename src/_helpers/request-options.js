c252b1709e86d08a4f35c2b537fef01922aca013
Ali-haïdar Atia <aatia@dawan.fr>
2021-11-23 13:26:29 +0000

    Update request-options.js

--- src/_helpers/request-options.js
+++ src/_helpers/request-options.js
@@ -10,8 +10,9 @@ function headers() {
         headers: {
             ...authHeader,
             'Content-Type': ['application/json', 'multipart/form-data'],
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods' :'DELETE, POST, GET, OPTIONS'
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*", 
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
         }
     }
 }
