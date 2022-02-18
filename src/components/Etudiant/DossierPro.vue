f01da207b7c7ad0fb8b712d02fa01a281d4f5f19
Remy <remy.charpentier89@gmail.com>
2022-02-18 16:57:33 +0100

    retrait du menu burger

--- src/components/Etudiant/DossierPro.vue
+++ src/components/Etudiant/DossierPro.vue
@@ -1,48 +1,55 @@
 <template>
-<div class="container">
+  <div class="container">
     <h5>Constituer un dossier professionnel</h5>
 
     <!-- select -->
     <select v-model="filter1" class="form-select">
-            <option v-for="choice in entriesSelect1" :key="choice">{{ choice }}</option>
+      <option v-for="choice in entriesSelect1" :key="choice">
+        {{ choice }}
+      </option>
     </select>
     <div id="div-label"><label for="">Activités types</label></div>
-    <SelectCDA v-if="filter1 == 'Concepteur développeur d\'applications'"/>
-    <SelectMPIL v-if="filter1 == 'Manager de projet en ingénierie logicielle'"/>
-</div>
+    <SelectCDA v-if="filter1 == 'Concepteur développeur d\'applications'" />
+    <SelectMPIL
+      v-if="filter1 == 'Manager de projet en ingénierie logicielle'"
+    />
+  </div>
 </template>
 
 <script>
 import SelectCDA from "@/components/Etudiant/SelectCDA.vue";
 import SelectMPIL from "@/components/Etudiant/SelectMPIL.vue";
 export default {
-   name: "Selects",
-   components: {
-       SelectCDA,
-       SelectMPIL
-    },
+  name: "Selects",
+  components: {
+    SelectCDA,
+    SelectMPIL,
+  },
   data: function () {
     return {
-      entriesSelect1: ["--Cursus--", "Concepteur développeur d'applications", "Manager de projet en ingénierie logicielle"],
+      entriesSelect1: [
+        "--Cursus--",
+        "Concepteur développeur d'applications",
+        "Manager de projet en ingénierie logicielle",
+      ],
       entriesSelect2: [],
       filter1: "--Cursus--",
     };
   },
 };
-
 </script>
 
 <style scoped>
-    #div-label {
-        margin-top: 10px;
-    }
-    h5 {
-        margin: 0 0 30px 0;
-    }
-    .container {
-        margin: 99px 0 0 421px;
-    }
-    select {
-        display: block;
-    }
+#div-label {
+  margin-top: 10px;
+}
+h5 {
+  margin: 0 0 30px 0;
+}
+.container {
+  margin: 99px 0 0 421px;
+}
+select {
+  display: block;
+}
 </style>
--- src/components/Etudiant/SelectCDA.vue
+++ src/components/Etudiant/SelectCDA.vue
@@ -2,13 +2,13 @@
   <div>
     <select class="form-select">
       <option value="cda">Activité type 1 CDA</option>
-      <option value="cda">Option 1</option>
+      <option value="cda" v-b-modal.modal-1>Option 1</option>
       <option value="cda">Option 2</option>
       <option value="cda">Option 3</option>
     </select>
     <select class="form-select">
       <option value="cda">Activité type 2 CDA</option>
-      <option value="cda">Option 1</option>
+      <option value="cda" >Option 1</option>
       <option value="cda">Option 2</option>
       <option value="cda">Option 3</option>
     </select>
@@ -24,6 +24,13 @@
       <option value="mpil">Exemple 2</option>
       <option value="mpil">Exemple 3</option>
     </select>
+       <!-- modal -->
+    <div>
+      <b-button v-b-modal.modal-1>test</b-button>
+      <b-modal id="modal-1" title="BootstrapVue">
+        <p class="my-4">Hello from modal!</p>
+      </b-modal>
+    </div>
   </div>
 </template>
 
