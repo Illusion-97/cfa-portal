273885da7158567d138a262021e3b08bfaebfd2c
Remy <remy.charpentier89@gmail.com>
2022-02-18 11:41:27 +0100

    creer dossier pro

---
+++ src/components/Etudiant/SelectMPIL.vue
@@ -0,0 +1,47 @@
+<template>
+     <div>
+    <select class="form-select">
+      <option value="mpil">Activité type 1 MPIL</option>
+      <option value="mpil">Option 1</option>
+      <option value="mpil">Option 2</option>
+      <option value="mpil">Option 3</option>
+    </select>
+    <select class="form-select">
+      <option value="mpil">Activité type 2 MPIL</option>
+      <option value="mpil">Option 1</option>
+      <option value="mpil">Option 2</option>
+      <option value="mpil">Option 3</option>
+    </select>
+    <select class="form-select">
+      <option value="mpil">Activité type 3 MPIL</option>
+      <option value="mpil">Option 1</option>
+      <option value="mpil">Option 2</option>
+      <option value="mpil">Option 3</option>
+    </select>
+    <select class="form-select">
+    <option value="mpil">Annexes</option>
+      <option value="mpil">Ajouter une expérience professionnelle</option>
+      <option value="mpil">Exemple 2</option>
+      <option value="mpil">Exemple 3</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+   name: "Selects",
+  data: function () {
+    return {
+    
+    };
+  },
+}
+</script>
+
+<style scoped>
+  select{
+    display:block;
+    min-width: 355px;
+    max-width: 355px;
+}
+</style>
