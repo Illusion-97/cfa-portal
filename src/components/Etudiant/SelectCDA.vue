34f416579dea4e6caf3c2a9313875968a948a2fc
unknown <wpires@dawan.fr>
2022-02-18 17:14:25 +0100

    delete modal test inutile

--- src/components/Etudiant/SelectCDA.vue
+++ src/components/Etudiant/SelectCDA.vue
@@ -24,13 +24,6 @@
       <option >Exemple 2</option>
       <option >Exemple 3</option>
     </select>
-       <!-- modal -->
-    <div>
-      <b-button v-b-modal.modal-1>test</b-button>
-      <b-modal id="modal-1" title="BootstrapVue">
-        <p class="my-4">Hello from modal!</p>
-      </b-modal>
-    </div>
   </div>
 </template>
 
