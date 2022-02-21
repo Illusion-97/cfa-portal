9ae08cb0bd87a3790938d340f76bcb06d98d4943
lionel <lbos@jehann.fr>
2022-02-21 12:10:55 +0100

    refacto dossier pro etudiant

--- src/components/Etudiant/DossierPro.vue
+++ src/components/Etudiant/DossierPro.vue
@@ -2,15 +2,69 @@
   <div class="container">
     <h5>Constituer un dossier professionnel</h5>
 
-    <!-- select -->
-
+    <!-- select Principal-->
     <select v-model="select1" class="form-select">
             <option v-for="choice in entriesSelect1" :key="choice">{{ choice }}</option>
     </select>
     <div id="div-label"><label for="">Activités types</label></div>
-    <SelectCDA v-if="select1 == 'Concepteur développeur d\'applications'" @modal="$bvModal.show('exp-pro-modal')"/>
-    <SelectMPIL v-if="select1 == 'Manager de projet en ingénierie logicielle'" @modal="$bvModal.show('exp-pro-modal')"/>
+    
+    <!-- SELECT CDA-->
+    <div v-if="select1 == 'Concepteur développeur d\'applications'">
+      <select class="form-select">
+        <option value="cda">Activité type 1 CDA</option>
+        <option value="cda">Option 1</option>
+        <option value="cda">Option 2</option>
+        <option value="cda">Option 3</option>
+      </select>
+      <select class="form-select">
+        <option value="cda">Activité type 2 CDA</option>
+        <option value="cda">Option 1</option>
+        <option value="cda">Option 2</option>
+        <option value="cda">Option 3</option>
+      </select>
+        <select class="form-select">
+        <option value="mpil">Activité type 3 CDA</option>
+        <option value="mpil">Option 1</option>
+        <option value="mpil">Option 2</option>
+        <option value="mpil">Option 3</option>
+      </select>
+      <select @change="checkAnnexesCDA" v-model="annexesCDA" class="form-select">
+        <option value="null">Annexes</option>
+        <option value="experience">Ajouter une expérience professionnelle</option>
+        <option >Exemple 2</option>
+        <option >Exemple 3</option>
+      </select>
+  </div>
 
+    <!-- SELECT MPIL-->
+    <div v-if="select1 == 'Manager de projet en ingénierie logicielle'">
+      <select class="form-select">
+      <option value="mpil">Activité type 1 MPIL</option>
+      <option value="mpil">Option 1</option>
+      <option value="mpil">Option 2</option>
+      <option value="mpil">Option 3</option>
+      </select>
+      <select class="form-select">
+      <option value="mpil">Activité type 2 MPIL</option>
+      <option value="mpil">Option 1</option>
+      <option value="mpil">Option 2</option>
+      <option value="mpil">Option 3</option>
+      </select>
+      <select class="form-select">
+      <option value="mpil">Activité type 3 MPIL</option>
+      <option value="mpil">Option 1</option>
+      <option value="mpil">Option 2</option>
+      <option value="mpil">Option 3</option>
+      </select>
+      <select @change="checkAnnexesMPIL" v-model="annexesMPIL" class="form-select">
+      <option value="null">Annexes</option>
+      <option value="experience">Ajouter une expérience professionnelle</option>
+      <option >Exemple 2</option>
+      <option >Exemple 3</option>
+      </select>
+    </div>
+
+    <!--modale Nouvelle experience professionnelle-->
     <b-modal id="exp-pro-modal"
               title="Exemple de compétence : Activité-type 3 -CDA"
               centered
@@ -36,14 +90,10 @@
 </template>
 
 <script>
-import SelectCDA from "@/components/Etudiant/SelectCDA.vue";
-import SelectMPIL from "@/components/Etudiant/SelectMPIL.vue";
 export default {
-  name: "Selects",
-  components: {
-    SelectCDA,
-    SelectMPIL,
-  },
+   name: "Selects",
+   components: {
+    },
   data: function () {
     return {
       entriesSelect1: [
@@ -52,12 +102,21 @@ export default {
         "Manager de projet en ingénierie logicielle",
       ],
       entriesSelect2: [],
-      select1: "--Cursus--"
+      select1: "--Cursus--",
+      annexesCDA: null,
+      annexesMPIL: null
     };
   },
   methods: {
-    showModal: function () {
-      this.$bvModal.show("experience-pro-modal");
+    checkAnnexesCDA: function () {
+      if (this.annexesCDA == "experience") {
+        this.$bvModal.show('exp-pro-modal');
+      }
+    },
+    checkAnnexesMPIL: function () {
+      if (this.annexesMPIL == "experience") {
+        this.$bvModal.show('exp-pro-modal');
+      }
     },
     save() {
 
