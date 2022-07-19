b49401b736f715a76cc9164d454c9adc52e9e293
Nicolas <npotier@dawan.fr>
2022-07-19 09:46:31 +0200

    edit promotion maj btn en cours  + liste et details

--- src/components/Navigation/VerticalNavbar.vue
+++ src/components/Navigation/VerticalNavbar.vue
@@ -65,6 +65,7 @@
             <!-- <router-link class="nav-item" :to="{name:'all-intervention'}">Interventions</router-link> -->
             <router-link class="nav-item" :to="{ name: 'admin_entreprise_list' }">Entreprise</router-link>
             <router-link class="nav-item" :to="{ name: 'admin_centreFormation_list' }">Centre de Formation</router-link>
+            <!-- <router-link class="nav-item" :to="{name: 'admin_projet_list' }">Projets</router-link> -->
             <router-link class="nav-item" :to="{ name: 'admin_adresse_list' }">Adresse</router-link>
             <!-- <router-link class="nav-item" :to="{ name: 'admin_etudiant_cerfa' }">Cerfa</router-link> -->
           </b-nav>
