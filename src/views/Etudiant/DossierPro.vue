bd94e4897baa4e8c40aa77d540409703b0014804
Remy <remycha@msn.com>
2022-08-30 12:02:19 +0200

    dossier pro et exp pro enregistré, pas encore lié à l'étudiant

--- src/views/Etudiant/DossierPro.vue
+++ src/views/Etudiant/DossierPro.vue
@@ -2,180 +2,117 @@
   <div class="container">
     <h5>
       Constituer un dossier professionnel :
-      <span>{{ data.item.titre }}</span>
+      <span>{{  data.item.titre  }}</span>
     </h5>
 
     <div v-for="(item, index) in activitesByCursus" :key="index">
       <!-- ACTIVITES TYPES SELECTEURS -->
-      <h6>Activité type {{ index + 1 }} : {{ item.libelle }}</h6>
+      <h6>Activité type {{  index + 1  }} : {{  item.libelle  }}</h6>
 
-      <b-form-select
-        v-model="item[index]"
-        :options="optionsAT(item)"
-        @change="getValue"
-      ></b-form-select>
+      <b-form-select v-model="item[index]" :options="optionsAT(item)" @change="getValue"></b-form-select>
       <br />
     </div>
 
     <!-- ACTIVITES TYPES MODALE -->
-    <b-modal
-      id="exp-pro-modal"
-      size="xl"
-      :title="
-        'Compétence professionnelle : ' +
-        activiteInModal.libelle 
-      "
-      centered
-      scrollable
-      no-close-on-esc
-      @hidden="resetModal"
-      hide-footer
-    >
+    <b-modal id="exp-pro-modal" size="xl" :title="
+      'Compétence professionnelle : ' +
+      compInModal.libelle
+    " centered scrollable no-close-on-esc @hidden="resetModal" hide-footer>
       <!-- FORMULAIRE -->
       <b-form @submit="onSubmit">
         <!-- ACCORDEON EXP 1 -->
         <b-card no-body class="mb-1">
           <b-card-header header-tag="header" class="p-1" role="tab">
-            <b-button
-              block
-              v-b-toggle.accordion-1
-              variant="primary"
-              class="titre-details-modal volets"
-              >1. Décrivez les tâches réalisées ou opérations que vous avez
+            <b-button block v-b-toggle.accordion-1 variant="primary" class="titre-details-modal volets">1. Décrivez les
+              tâches réalisées ou opérations que vous avez
               effectué et dans quelles conditions.
               <i class="bi bi-caret-down-square-fill"></i>
             </b-button>
           </b-card-header>
-          <b-collapse
-            id="accordion-1"
-            visible
-            accordion="my-accordion"
-            role="tabpanel"
-          >
+          <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+
+            <!-- INSERT EXP -->
             <b-card-body>
-
-              <!-- TEST -->
-              <!-- <b-form-input
-                id="test1"
-                v-model="data.item.id"
-                name="cursusId"
-                placeholder="Cursus Id"
-              ></b-form-input>
-              <b-form-input
-                id="test1"
-                name="cursusTitre"
-                placeholder="Cursus Titre"
-                v-model="data.item.titre"
-              ></b-form-input> -->
-
-              <!-- INSERT EXP -->
-              <b-form-input
-                id="exp1"
-                v-model="form.experienceProfessionnelles.tacheRealisee"
-                name="tacheRealisee"
-                placeholder="Tâches réalisées"
-              ></b-form-input>
-
-              <!-- <b-form-input
-                      id="exp1"
-                      placeholder="TACHES REALISEES"
-                      required
-                    ></b-form-input> -->
-
-              <!-- <b-card-text>{{ text }}</b-card-text> -->
+              <b-form-input id="exp1" v-model="form.experienceProfessionnelles.tacheRealisee" name="tacheRealisee"
+                placeholder="Tâches réalisées"></b-form-input>
             </b-card-body>
+
           </b-collapse>
         </b-card>
 
         <!-- ACCORDEON EXP 2 -->
         <b-card no-body class="mb-1">
           <b-card-header header-tag="header" class="p-1" role="tab">
-            <b-button
-              block
-              v-b-toggle.accordion-2
-              variant="primary"
-              class="titre-details-modal volets"
-              >2. Précisez les moyens utilisés.
+            <b-button block v-b-toggle.accordion-2 variant="primary" class="titre-details-modal volets">2. Précisez les
+              moyens utilisés.
               <i class="bi bi-caret-down-square-fill"></i>
             </b-button>
           </b-card-header>
           <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+
+            <!-- INSERT EXP -->
             <b-card-body>
-              <b-form-textarea
-                id="textarea-rows"
-                placeholder=""
-                rows="8"
-              ></b-form-textarea>
+              <b-form-input id="exp2" v-model="form.experienceProfessionnelles.moyenUtilise" name="moyenUtilise"
+                placeholder="Moyens utilisés"></b-form-input>
             </b-card-body>
+
           </b-collapse>
         </b-card>
 
         <!-- ACCORDEON EXP 3 -->
         <b-card no-body class="mb-1">
           <b-card-header header-tag="header" class="p-1" role="tab">
-            <b-button
-              block
-              v-b-toggle.accordion-3
-              variant="primary"
-              class="titre-details-modal volets"
-              >3. Avec qui avez-vous travaillé ?
+            <b-button block v-b-toggle.accordion-3 variant="primary" class="titre-details-modal volets">3. Avec qui
+              avez-vous travaillé ?
               <i class="bi bi-caret-down-square-fill"></i>
             </b-button>
           </b-card-header>
           <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
+
+            <!-- INSERT EXP -->
             <b-card-body>
-              <b-form-textarea
-                id="textarea-rows"
-                placeholder=""
-                rows="8"
-              ></b-form-textarea>
+              <b-form-input id="exp3" v-model="form.experienceProfessionnelles.collaborateur" name="collaborateur"
+                placeholder="Collaborateurs"></b-form-input>
             </b-card-body>
+
           </b-collapse>
         </b-card>
 
         <!-- ACCORDEON EXP 4 -->
         <b-card no-body class="mb-1">
           <b-card-header header-tag="header" class="p-1" role="tab">
-            <b-button
-              block
-              v-b-toggle.accordion-4
-              variant="primary"
-              class="titre-details-modal volets"
-              >4. Précisez le contexte.
+            <b-button block v-b-toggle.accordion-4 variant="primary" class="titre-details-modal volets">4. Précisez le
+              contexte.
               <i class="bi bi-caret-down-square-fill"></i>
             </b-button>
           </b-card-header>
           <b-collapse id="accordion-4" accordion="my-accordion" role="tabpanel">
+
+            <!-- INSERT EXP -->
             <b-card-body>
-              <b-form-textarea
-                id="textarea-rows"
-                placeholder=""
-                rows="8"
-              ></b-form-textarea>
+              <b-form-input id="exp4" v-model="form.experienceProfessionnelles.contexte" name="contexte"
+                placeholder="Contexte"></b-form-input>
             </b-card-body>
+
           </b-collapse>
         </b-card>
 
         <!-- ACCORDEON EXP 5 -->
         <b-card no-body class="mb-1">
           <b-card-header header-tag="header" class="p-1" role="tab">
-            <b-button
-              block
-              v-b-toggle.accordion-5
-              variant="primary"
-              class="titre-details-modal volets"
-              >5. Informations complémentaires (facultatif).
+            <b-button block v-b-toggle.accordion-5 variant="primary" class="titre-details-modal volets">5. Informations
+              complémentaires (facultatif).
               <i class="bi bi-caret-down-square-fill"></i>
             </b-button>
           </b-card-header>
           <b-collapse id="accordion-5" accordion="my-accordion" role="tabpanel">
+
+            <!-- INSERT EXP -->
             <b-card-body>
-              <b-form-textarea
-                id="textarea-rows"
-                placeholder=""
-                rows="8"
-              ></b-form-textarea>
+              <b-form-input id="exp5" v-model="form.experienceProfessionnelles.information" name="information"
+                placeholder="Informations"></b-form-input>
             </b-card-body>
+
           </b-collapse>
         </b-card>
 
@@ -208,37 +145,73 @@ export default {
       activites: [],
       options: [],
       selectActivite: [],
-      activiteInModal: [],
+      compInModal: [],
       activitesByCursus: [],
+
+      tempActivite: [],
+      tempCompetence: [],
 
       form: {
         id: 0,
         nom: "",
+
         cursus: {
-          id: 0,
+          id: 1,
           titre: "",
 
-          activiteTypes: {
-            id: 0,
+          activiteTypes: [{
+            id: 1,
             libelle: "",
 
-            competenceProfessionnelles: {
-              id: 0,
-              libelle: "",
-            },
-          },
+            competenceProfessionnelles: [{
+              id: 1,
+              libelle: ""
+            }],
+          }],
         },
 
-        experienceProfessionnelles: {
+        experienceProfessionnelles: [{
           id: 0,
-          tacheRealisee: "",
+          tacheRealisee: "aaa",
           moyenUtilise: "",
           collaborateur: "",
           contexte: "",
           information: "",
-          competenceProfessionnelleId: 0,
-        },
+          competenceProfessionnelleId: 1
+        }],
       },
+
+      // A FAIRE
+      // form: {
+      //   id: 0,
+      //   nom: "",
+
+      //   cursus: {
+      //     id: 1,
+      //     titre: "cursus non dynamique",
+
+      //     activiteTypes: [{
+      //       id: 1,
+      //       libelle: "Activite type non dynamique",
+
+      //       competenceProfessionnelles: [{
+      //         id: this.tempCompetence.id,
+      //         libelle: this.tempCompetence.libelle
+      //       }],
+      //     }],
+      //   },
+
+      //   experienceProfessionnelles: [{
+      //     id: 0,
+      //     tacheRealisee: this.form.experienceProfessionnelles.tacheRealisee,
+      //     moyenUtilise: this.form.experienceProfessionnelles.moyenUtilise,
+      //     collaborateur: this.form.experienceProfessionnelles.collaborateur,
+      //     contexte: this.form.experienceProfessionnelles.contexte,
+      //     information: this.form.experienceProfessionnelles.information,
+      //     competenceProfessionnelleId: this.tempCompetence.id
+      //   }],
+      // },
+
     };
   },
 
@@ -249,15 +222,16 @@ export default {
       this.selectActivite = null;
     },
 
-    // OBJET ACTIVITE DANS MODAL
+    // LANCE LA MODALE DE LA COMPETENCE CHOISIE
     getValue(value) {
       console.log("getValue > " + value);
       console.dir("getValue > " + JSON.stringify(value, null, 4));
-      this.activiteInModal = value;
+      this.compInModal = value;
       console.dir(
-        "activiteInModal > " + JSON.stringify(this.activiteInModal, null, 4)
+        "compInModal > " + JSON.stringify(this.compInModal, null, 4)
       );
       this.$bvModal.show("exp-pro-modal");
+      this.tempCompetence = value;
     },
 
     // OPTIONS DES ACTIVITES TYPES
@@ -284,17 +258,50 @@ export default {
     onSubmit(event) {
       event.preventDefault();
       console.log("ici"),
-        dossierProfessionnelApi.saveDossierProfessionnel(
-          // console.dir("form > " + JSON.stringify(this.form, null, 4)),
-          // console.log("etudiant id > " + this.$store.getters.getUtilisateur.etudiantDto.id),
-          this.form,
-          1,
-        );
-        alert('dossier crée')
+        dossierProfessionnelApi
+          .saveDossierProfessionnel(
+
+            // console.dir("form > " + JSON.stringify(this.form, null, 4)),
+            // console.log("etudiant id > " + this.$store.getters.getUtilisateur.etudiantDto.id),
+
+            this.$store.getters.getUtilisateur.etudiantDto.id,
+            // this.form
+            {
+              id: 0,
+              nom: "",
+
+              cursus: {
+                id: 0,
+                titre: "",
+
+                activiteTypes: [{
+                  id: 0,
+                  libelle: "",
+
+                  competenceProfessionnelles: [{
+                    id: this.tempCompetence.id,
+                    libelle: this.tempCompetence.libelle
+                  }],
+                }],
+              },
+
+              experienceProfessionnelles: [{
+                id: 0,
+                tacheRealisee: this.form.experienceProfessionnelles.tacheRealisee,
+                moyenUtilise: this.form.experienceProfessionnelles.moyenUtilise,
+                collaborateur: this.form.experienceProfessionnelles.collaborateur,
+                contexte: this.form.experienceProfessionnelles.contexte,
+                information: this.form.experienceProfessionnelles.information,
+                competenceProfessionnelleId: this.tempCompetence.id
+              }],
+            },
+
+          );
+      alert('dossier crée')
     },
 
-    test() {},
-    save() {},
+    test() { },
+    save() { },
   },
 
   created() {
@@ -316,13 +323,16 @@ export default {
 footer {
   display: none;
 }
+
 .custom-select {
   margin-bottom: 5px;
 }
+
 .bi {
   position: relative;
   top: 2px;
 }
+
 .volets {
   display: flex;
   justify-content: space-between;
@@ -336,13 +346,16 @@ footer {
 #div-label {
   margin-top: 10px;
 }
+
 h5 {
   margin: 0 0 30px 0;
   font-weight: bolder;
 }
+
 .container {
   margin: 89px 0 0 421px;
 }
+
 select {
   display: block;
 }
