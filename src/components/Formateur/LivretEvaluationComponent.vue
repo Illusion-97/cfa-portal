28ee6bcec06d780560819c081871883a8ffd6bda
Nicolas Potier <npotier@dawan.fr>
2023-01-12 12:00:25 +0100

    corr ortho

--- src/components/Formateur/LivretEvaluationComponent.vue
+++ src/components/Formateur/LivretEvaluationComponent.vue
@@ -2,7 +2,7 @@
   <section v-if="(livretEvaluation != null)">
     <div>
       <h3 class="m-4  text-center">Etât du livret : {{ livretEvaluation.etat | etatLivret }} </h3>
-      <b-button v-if="livretEvaluation.etat != 'ENATTENTEDEVALIDATION'" variant="primary" class="m-4" @click="generer">Télécharger livret d'évaluation</b-button>
+      <b-button v-if="livretEvaluation.etat != 'ENATTENTEDEVALIDATION'" variant="primary" class="m-4" @click="generer">Télécharger le livret d'évaluation</b-button>
       <b-card no-body class="mb-1" v-for="(eva, i ) in atEvaluations" :key="eva.at.id">
         <b-card-header header-tag="header" class="p-1" role="tab">
           <b-button block v-b-toggle.accordion-1 class="btn-accordion">{{ eva.at.libelle }}</b-button>
