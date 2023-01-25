87210bb44fa4543459819ae3ae5d145c8699fa1d
HugoTres93 <torresmartins.hugo@gmail.com>
2023-01-25 13:46:31 +0100

    pas de bare de recherche et de pagination

---
+++ src/views/Tuteur/AcceuilTuteur.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+    </section>
+</template>
+
+<script>
+
+
+export default {
+  name: "Tuteur",
+  data() {
+    return {
+
+
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
