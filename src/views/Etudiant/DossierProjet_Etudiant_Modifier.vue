6ea3c92679c577a9df0f3aac577280ed6bca2d28
hugo Torres <htorres@dawan.fr>
2023-03-03 16:06:02 +0100

    container dissier projet modifier

--- src/views/Etudiant/DossierProjet_Etudiant_Modifier.vue
+++ src/views/Etudiant/DossierProjet_Etudiant_Modifier.vue
@@ -492,7 +492,7 @@ export default {
 };
 </script>
 
-<style scoped src="@/assets/styles/StyleEtudiant.css" >
+<style lang="scss"  >
 /* margin : haut droit bas gauche */
 #main-cr-prj {
   background-color: none;
@@ -537,7 +537,10 @@ export default {
         padding: 5px 5px 5px 5px;
     } */
 
+#container {
 
+  display: block;
+}
 
 #container2 {
   display: flex;
