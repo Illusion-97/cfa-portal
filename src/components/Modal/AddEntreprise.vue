f704256866b3904f53b07797ba29e9f9a088bfc6
hugo Torres <htorres@dawan.fr>
2023-05-12 15:32:43 +0200

    formulaire addAdresse

---
+++ src/components/Modal/AddEntreprise.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+            <v-app>
+                <form v-on:submit.prevent="addTuteur">
+
+                </form>
+            </v-app>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+    methods: {
+        addAdresse() {
+
+        },
+    },
+}
+</script>
