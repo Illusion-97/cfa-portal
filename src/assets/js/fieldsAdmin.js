15d138a27f1ca1771ac67b6ea9cfd5684da3c9f9
hugo Torres <htorres@dawan.fr>
2023-06-01 16:39:00 +0200

    modif fieldsAdmin

--- src/assets/js/fieldsAdmin.js
+++ src/assets/js/fieldsAdmin.js
@@ -27,49 +27,56 @@ export const utilisateursFields = [
 ];
 
 export const etudiantsFields = [
-    { key: "nom", label: "Nom de l'étudiant" },
-    { key: "prenom", label: "Prénom de l'étudiant" },
-    { key: "adresse", label: "Adresse" },
-    { key: "presence", label: "Feuille de présence"},
-  
-  ];
+  { key: "nom", label: "Nom de l'étudiant" },
+  { key: "prenom", label: "Prénom de l'étudiant" },
+  { key: "adresse", label: "Adresse" },
+  { key: "presence", label: "Feuille de présence" },
 
-  export const noteFields = [
-    { key: "nom", label: "Nom de l'étudiant" },
-    { key: "note_obtenu", label: "Note" },
-    { key: "observations", label: "Observations" },
-  ];
-  
-  export const projetsFields = [
-    { key: "nom", label: "Nom du groupe" },
-    { key: "description", label: "Description" },
-    { key: "cahierDesCharges", label: "Cahier des charges" },
-    { key: "groupe", label: "Groupe"},
-    "modifier",
-    "supprimer",
-  
-  ];
-  
-  export const promotionsFields = [
-    { key: "nom", label: "Nom de promotion" },
-    { key: "etudiant", label: "Etudiant" },
-    { key: "ProgrammeDeCours", label: "Programme de cours" },
-    { key: "Cours", label: "Cours"},
-  
-  ];
-  
-  export const groupesFields = [
-    { key: "nom", label: "Nom du groupe" },
-    "modifier",
-    "supprimer",
-  
-  ];
-  
-  export const examensFields = [
-    { key: "enonce", label: "Enonce" },
-    { key: "cursus", label: "Cursus" },
-    { key: "formation", label: "Formation" },
-    "modifier",
-    "supprimer",
-  
-  ];
+];
+
+export const noteFields = [
+  { key: "nom", label: "Nom de l'étudiant" },
+  { key: "note_obtenu", label: "Note" },
+  { key: "observations", label: "Observations" },
+];
+
+export const projetsFields = [
+  { key: "nom", label: "Nom du groupe" },
+  { key: "description", label: "Description" },
+  { key: "cahierDesCharges", label: "Cahier des charges" },
+  { key: "groupe", label: "Groupe" },
+  "modifier",
+  "supprimer",
+
+];
+
+export const promotionsFields = [
+  { key: "nom", label: "Nom de promotion" },
+  { key: "etudiant", label: "Etudiant" },
+  { key: "ProgrammeDeCours", label: "Programme de cours" },
+  { key: "Cours", label: "Cours" },
+
+];
+
+export const groupesFields = [
+  { key: "nom", label: "Nom du groupe" },
+  "modifier",
+  "supprimer",
+
+];
+
+export const examensFields = [
+  { key: "enonce", label: "Enonce" },
+  { key: "cursus", label: "Cursus" },
+  { key: "formation", label: "Formation" },
+  "modifier",
+  "supprimer",
+
+];
+
+export const AdresseFields = [
+  { key: "libelle", text: "Libelle", sortable: false },
+  { key: "ville", text: "Ville", sortable: false },
+  { key: "codePostal", text: "Code Postal", sortable: false },
+  { key: "Actions", text: "Actions", thStyle: { width: "20%" }, thClass: "text-center" },
+];
