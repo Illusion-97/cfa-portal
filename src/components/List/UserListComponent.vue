b62005aa840f8b200273555d9f6d5c8eea9aaf60
Nicolas <npotier@dawan.fr>
2023-08-28 11:57:24 +0200

    pagination Projet ok

--- src/components/List/UserListComponent.vue
+++ src/components/List/UserListComponent.vue
@@ -135,10 +135,21 @@
       </b-form>
     </b-modal>
 
-    <paginate class="customPagination" :page-count="pageCount" :page-range="1" :margin-pages="2"
-      :click-handler="pageChange" :prev-text="'Prev'" :next-text="'Next'" :container-class="'pagination float-right'"
-      :page-class="'page-item'" :page-link-class="'page-link'" :prev-class="'page-item'" :next-class="'page-item'"
-      :prev-link-class="'page-link'" :next-link-class="'page-link'" :active-class="'active'">
+    <paginate 
+      class="customPagination" 
+      :page-count="pageCount" 
+      :page-range="1" :margin-pages="2"
+      :click-handler="pageChange" 
+      :prev-text="'Prev'" 
+      :next-text="'Next'" 
+      :container-class="'pagination float-right'"
+      :page-class="'page-item'" 
+      :page-link-class="'page-link'" 
+      :prev-class="'page-item'" 
+      :next-class="'page-item'"
+      :prev-link-class="'page-link'" 
+      :next-link-class="'page-link'" 
+      :active-class="'active'">
     </paginate>
   </div>
 </template>
